407f43c41563cbddacc0b0dd957c27bd1f3cec6b
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-08-25 20:59:31 -0700

    Define consts for local storage and page limit

--- consts/spotify.ts
+++ consts/spotify.ts
@@ -1,1 +1,6 @@
 export const SPOT_PLAYLIST_ITER_INT = 1;
+export const SPOT_PLAYLIST_PAGE_LIMIT = 49;
+export const LOCAL_EXPIRES = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_expires`;
+export const LOCAL_END = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_end`;
+export const LOCAL_USER_LISTS = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_user_lists`;
+export const LOCAL_CUSTOM_LISTS = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_custom_lists`;
