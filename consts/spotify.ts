a12ed758cce86ebf88955ff875c8f67daf6145e1
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-09-18 17:03:00 -0700

    Misc. spacing thing for a const

--- consts/spotify.ts
+++ consts/spotify.ts
@@ -1,6 +1,6 @@
 export const SPOT_PLAYLIST_ITER_INT = 10;
 export const SPOT_PLAYLIST_PAGE_LIMIT = 49;
-export const SPOT_LOGIN_WINDOW = 50*60;
+export const SPOT_LOGIN_WINDOW = 50 * 60;
 export const LOCAL_EXPIRES = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_expires`;
 export const LOCAL_END = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_end`;
 export const LOCAL_USER_LISTS = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_user_lists`;
