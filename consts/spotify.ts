03331ea054fa5aa0c228c4c19477b9c76564d4b1
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-09-18 17:02:41 -0700

    Add consts for differ action types
    Add const for spotify url base

--- consts/spotify.ts
+++ consts/spotify.ts
@@ -5,3 +5,11 @@ export const LOCAL_EXPIRES = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_expires`;
 export const LOCAL_END = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_end`;
 export const LOCAL_USER_LISTS = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_user_lists`;
 export const LOCAL_CUSTOM_LISTS = `${process.env.NEXT_PUBLIC_STORAGE_SALT}_custom_lists`;
+export const differTypesAndStrings = {
+	'adu': "Add what's different to the target",
+	'odu': "Delete everything except what's unique to the differ",
+	'otu': "Delete everything except what's unique to the target",
+	'bu': 'Delete all similarities but keep all differences',
+	'stu': "Keep only what's similar to the differ"
+};
+export const SPOT_URL_BASE = 'https://api.spotify.com/v1/'
