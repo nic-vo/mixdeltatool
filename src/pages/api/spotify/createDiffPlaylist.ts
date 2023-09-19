328227bf6edaffb16f2d5d16c600ab4da9bf2fa0
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-09-18 17:43:29 -0700

    Parse POST body with zod parser

--- src/pages/api/spotify/createDiffPlaylist.ts
+++ src/pages/api/spotify/createDiffPlaylist.ts
@@ -3,23 +3,33 @@ import { routeKeyRetriever } from '@lib/auth/accessKey';
 import { authOptions } from '@lib/auth/options';
 import { getServerSession } from 'next-auth';
 
+import { diffBodyParser } from '@lib/spotify/validators';
+
+import { ZodError } from 'zod/lib';
 import { NextApiResponse } from 'next';
 import { createDiffPlaylistApiRequest } from '@components/spotify/types';
 
-export default async function handler(req: createDiffPlaylistApiRequest, res: NextApiResponse) {
+export default async function handler(
+	req: createDiffPlaylistApiRequest, res: NextApiResponse
+) {
 	try {
 		// Validate req method
-		if (req.method !== 'POST')
-			throw { status: 405, error: 'POST only' };
-		// Validate query parameters
-		if (Object.keys(req.query).length !== 2
-			|| 'target' in req.query === false
-			|| 'differ' in req.query === false)
-			throw { status: 400, error: 'Bad request' };
-		// Spotify playlist id *SHOULD* be 22 chars, alphanumeric
-		if (/^[A-Za-z0-9]{22}$/.test(req.query.target) === false
-			|| /^[A-Za-z0-9]{22}$/.test(req.query.differ) === false)
-			throw { status: 422, error: 'Bad request' };
+		if (req.method !== 'POST') throw { status: 405, error: 'POST only' };
+		// Validate body and body values
+		let target, differ, actionType;
+		try {
+			const parsed = diffBodyParser.parse(req.body);
+			target = parsed.target;
+			differ = parsed.differ;
+			actionType = parsed.type;
+		} catch (e: any) {
+			const error = e as ZodError;
+			const map = new Map();
+			for (const issue of error.issues) map.set(issue.code, issue);
+			if (map.has('unrecognized_keys') === true)
+				throw { status: 400, error: 'Bad request' };
+			else throw { status: 422, error: 'Bad request' };
+		};
 
 		// Check next-auth session
 		const session = await getServerSession(req, res, authOptions);
@@ -31,23 +41,19 @@ export default async function handler(req: createDiffPlaylistApiRequest, res: Ne
 		let token = null;
 		// Custom access token retriever outside of next-auth flow
 		// Network failure is possible here
-		try {
-			token = await routeKeyRetriever(session.user.id);
-		} catch {
-			throw { status: 502, error: 'Network error' };
-		};
+		try { token = await routeKeyRetriever(session.user.id) }
+		catch { throw { status: 502, error: 'Network error' } };
 		// No token means that user account somehow unlinked => client redirect
 		if (token === null) throw { status: 401, error: 'Unauthorized' };
 
 		// Check if session is being accessed when access token might not be live
 		const { expires_at, access_token } = token;
-		if (expires_at === undefined
+		if ((expires_at === undefined || expires_at === null)
+			|| (access_token === undefined || access_token === null)
 			|| Date.now() - expires_at < 3600 - SPOT_LOGIN_WINDOW)
 			throw { status: 401, error: 'Unauthorized' };
 	} catch (e: any) {
 		return res.status(e.status || 500)
 			.json({ error: e.error || 'Unknown error' });
 	};
-
-	return res.status(200).json({ message: 'Success' });
 };
