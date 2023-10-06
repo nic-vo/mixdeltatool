9c63d6ed7be51549f9bb34c315e9c3ec8dea12f1
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-10-06 13:42:52 -0700

    Add new query parser for specific getter
    Was instantly throwing because I modified the original to account for the differ

--- lib/spotify/validators.ts
+++ lib/spotify/validators.ts
@@ -5,12 +5,20 @@ const pageQueryParser = z.object({
 	page: z.coerce.number().int().gte(0).lte(SPOT_PLAYLIST_PAGE_LIMIT)
 }).strict();
 
+
 const idParamParser = z.string().length(22).regex(/^[A-Za-z0-9]{22}$/);
 const typeParamParser = z.enum(['playlist', 'album']);
 
-const playlistIdTypeParser = z.object({
+const specificQueryParser = z.object({
 	id: idParamParser,
 	type: typeParamParser
+}).strict();
+
+const playlistIdTypeParser = z.object({
+	id: idParamParser,
+	type: typeParamParser,
+	name: z.string(),
+	owner: z.string()
 }).strict();
 
 const diffBodyParser = z.object({
@@ -21,6 +29,7 @@ const diffBodyParser = z.object({
 
 export {
 	pageQueryParser,
+	specificQueryParser,
 	playlistIdTypeParser,
 	diffBodyParser
 };
