30891194f691bd926045fdaea80823efa09fd808
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-11-22 12:12:15 -0800

    Add lorem ipsum for homepage copy and new link to tool

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
-import Link from 'next/link';
-
 export default function Home() {
 
 	return (
 		<main>
 			<h1>Homepage</h1>
-			<Link href='/spotify'>Spotify</Link>
+			<section>
+				<h2>What this is</h2>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra massa. Blandit cursus risus at ultrices mi tempus. Feugiat in ante metus dictum at tempor. Tincidunt augue interdum velit euismod in pellentesque. Eu turpis egestas pretium aenean pharetra magna ac. Curabitur vitae nunc sed velit dignissim sodales ut. Neque sodales ut etiam sit amet nisl purus in mollis. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam. Hendrerit dolor magna eget est. Nulla aliquet enim tortor at auctor. Mauris pharetra et ultrices neque. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Urna molestie at elementum eu facilisis sed odio morbi quis. Leo integer malesuada nunc vel risus commodo viverra maecenas.
+				</p>
+			</section>
+			<section>
+				<h2>Who's it for</h2>
+				<p>Praesent tristique magna sit amet. Euismod quis viverra nibh cras pulvinar mattis nunc. Eu augue ut lectus arcu bibendum at varius vel pharetra. Ut sem viverra aliquet eget sit amet. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Nec nam aliquam sem et tortor consequat id porta nibh. Volutpat ac tincidunt vitae semper. Arcu non odio euismod lacinia at quis. Elementum curabitur vitae nunc sed. Amet commodo nulla facilisi nullam vehicula. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Risus quis varius quam quisque id diam vel. Sem nulla pharetra diam sit. Tortor vitae purus faucibus ornare suspendisse sed nisi. Lacus luctus accumsan tortor posuere ac ut consequat. Turpis tincidunt id aliquet risus feugiat in. Lobortis mattis aliquam faucibus purus in massa tempor nec feugiat. Consequat mauris nunc congue nisi vitae suscipit tellus mauris.</p>
+			</section>
+			<br /><br /><br />
+			<a href='/spotify'>Here's a link to the tool</a>
 		</main>
 	);
-};
+}
