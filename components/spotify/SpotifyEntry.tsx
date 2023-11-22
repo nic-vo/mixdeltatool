17cdc973fa372c7f7b813ca12d04a2085eeaaf23
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-11-22 12:22:30 -0800

    Entry now imports custom header
    Simplify conditional render based on auth

--- components/spotify/SpotifyEntry.tsx
+++ components/spotify/SpotifyEntry.tsx
@@ -1,20 +1,16 @@
-import { signOut, useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
 import { SpotifyMain } from './dynamic';
+import Header from './Header/Header';
 
 export default function SpotifyEntry() {
 	const { data, status } = useSession({ required: true });
-	if (status === 'loading' || !data.user) return <h1>Loading...</h1>
 	if (status === 'authenticated' && data.user) {
-		const { email, image, name } = data.user;
 		return (
 			<>
-				<header>
-					<h1>Hello there {email?.split('@')[0] || name}!</h1>
-					{image !== null && <img src={image} alt='' />}
-					<button onClick={() => signOut()}>Sign out</button>
-				</header>
+				<Header />
 				<SpotifyMain />
 			</>
 		);
-	};
-};
+	}
+	return <h1>Loading...</h1>
+}
