37385dfe8eba026fa04e8bfba412843d870365d7
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-11-22 12:34:12 -0800

    Move main to Spotify Main

--- components/spotify/dynamic/SpotifyMain.tsx
+++ components/spotify/dynamic/SpotifyMain.tsx
@@ -3,15 +3,20 @@ import { UserPlaylistProvider } from './contexts/UserPlaylistProvider';
 import SpotifyRouter from './SpotifyRouter';
 
 const SpotifyMain = () => {
-
 	return (
-		<>
+		<main style={{
+			display: 'flex',
+			alignItems: 'center',
+			flexDirection: 'column',
+			width: '100svw'
+		}}>
 			<UserPlaylistProvider>
 				<SpecificPlaylistProvider>
 					<SpotifyRouter />
 				</SpecificPlaylistProvider>
 			</UserPlaylistProvider>
-		</>
-	)
+		</main>
+	);
 }
+
 export default SpotifyMain;
