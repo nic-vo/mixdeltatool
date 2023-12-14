78f0242bbb120f3670e873c8f7c1aa008222ee71
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-12-13 18:16:22 -0800

    SpotifyMain now contains the main turning point for authentication and user actions

--- components/spotify/dynamic/SpotifyMain.tsx
+++ components/spotify/dynamic/SpotifyMain.tsx
@@ -1,21 +1,29 @@
 import { SpecificPlaylistProvider } from './contexts/SpecificPlaylistProvider';
 import { UserPlaylistProvider } from './contexts/UserPlaylistProvider';
 import SpotifyRouter from './SpotifyRouter';
+import { signIn, useSession } from 'next-auth/react';
+
+import global from '@styles/globals.module.scss';
 
 const SpotifyMain = () => {
+	const { status } = useSession();
+
+	if (status === 'authenticated') return (
+		<UserPlaylistProvider>
+			<SpecificPlaylistProvider>
+				<SpotifyRouter />
+			</SpecificPlaylistProvider>
+		</UserPlaylistProvider>
+	)
 	return (
-		<main style={{
-			display: 'flex',
-			alignItems: 'center',
-			flexDirection: 'column',
-			width: '100svw'
-		}}>
-			<UserPlaylistProvider>
-				<SpecificPlaylistProvider>
-					<SpotifyRouter />
-				</SpecificPlaylistProvider>
-			</UserPlaylistProvider>
-		</main>
+		<>
+			<h1 style={{ textAlign: 'center', width: '100%' }}>Sign in please!</h1>
+			<div>
+				<button
+					onClick={() => signIn()}
+					className={global.emptyButton}>Sign in</button>
+			</div>
+		</>
 	);
 }
 
