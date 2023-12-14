8a1492f61972c8606f5e77e8ed58ae8b8164adce
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-12-13 18:14:07 -0800

    Add DeleteDialog, link to privacy policy and home to header
    Dynamically load the hidden content
    Add signIn button if user isn't authenticated

--- components/spotify/Header/Header.tsx
+++ components/spotify/Header/Header.tsx
@@ -1,26 +1,33 @@
 import { useState } from 'react';
-import { FaBars, FaHourglassHalf } from 'react-icons/fa';
+import { FaBars, FaHome, FaHourglassHalf } from 'react-icons/fa';
 
 import look from './Header.module.scss';
-import { useSession } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
 import dynamic from 'next/dynamic';
+import { Loading } from '@components/misc';
 
 const Hidden = dynamic(import('./HiddenContent'),
 	{
 		ssr: false,
-		loading: ({ error }) => {
-			return (error !== null ? <p>Please refresh page.</p> : <p>Loading...</p>)
-		}
-	})
+		loading: (props: {
+			error?: Error | null,
+			pastDelay?: boolean,
+			timedOut?: boolean
+		}) => <Loading
+				error={props.error}
+				pastDelay={props.pastDelay}
+				timedOut={props.timedOut} />
+	});
 
 const Content = () => {
 	const { data, status } = useSession();
 
 	return (
-		<section className={look.container}>
+		<>
 			<section className={look.info}>
+				{status !== 'authenticated' && <button onClick={() => signIn()}>Sign in</button>}
 				{
-					data !== null && data.user.image ?
+					data !== undefined && data !== null && data.user.image ?
 						<img
 							src={data.user.image}
 							alt='profile picture'
@@ -29,14 +36,14 @@ const Content = () => {
 						<div>Image here</div>
 				}
 				{
-					data !== null && data.user.name ?
+					data !== undefined && data !== null && data.user.name ?
 						<p>{data.user.name}</p>
 						:
 						<div>Name here</div>
 				}
 			</section>
 			{status === 'authenticated' ? <Hidden /> : <FaHourglassHalf />}
-		</section>
+		</>
 	);
 }
 
@@ -48,13 +55,18 @@ const Toggler = (props: {
 		`${look.header} ${look.active}` : look.header;
 
 	return (
-		<header className={classer}>
+		<header className={classer} onFocus={() => setToggle(true)}>
 			<button
 				onClick={() => setToggle(!toggle)}
+				onFocus={() => setToggle(true)}
 				className={look.toggler}><FaBars /></button>
 			<div className={look.returner}
-				onClick={() => setToggle(!toggle)} />
+				onClick={() => setToggle(false)} />
 			{props.children}
+			<section className={look.innerContainer}>
+				<a href='/privacypolicy' className={look.button}>Privacy Policy</a>
+				<a href='/' className={look.button} onBlur={() => setToggle(false)}><FaHome /></a>
+			</section>
 		</header>
 	);
 }
--- components/spotify/Header/HiddenContent.tsx
+++ components/spotify/Header/HiddenContent.tsx
@@ -1,107 +1,18 @@
-import { useState, useRef } from 'react';
-import { signIn, signOut } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 
 import look from './Header.module.scss';
+import DeleteDialog from './DeleteDialog';
 
 const HiddenContent = () => {
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState<string | null>(null);
-	const [success, setSuccess] = useState<boolean>(false);
-	const modalRef = useRef<HTMLDialogElement>(null);
-
-	const submitHandler = async (e: React.FormEvent) => {
-		e.preventDefault();
-		const data = new FormData(e.target as HTMLFormElement);
-		if (data.get('confirmation') !== 'superusers') {
-			setError('Try again');
-			return null;
-		}
-		setLoading(true);
-		setError(null);
-		try {
-			const res = await fetch(`/api/account/${process.env.NEXT_PUBLIC_ACCOUNT_ROUTE_OBFUS}`);
-			if (res.status === 401) signIn();
-			else if (res.ok === false) throw { message: 'Unknown error' };
-			setSuccess(true);
-			setTimeout(() => signOut(), 5000);
-		} catch (e: any) {
-			setError(e.message ? e.message : 'Unknown error');
-		}
-		setLoading(false);
-		return null;
-	}
-
 	return (
-		<>
-			<dialog ref={modalRef} className={look.modal}>
-				<form onSubmit={submitHandler}>
-					<label htmlFor='confirmation'>
-						To confirm account deletion request,
-						type "superusers" and hit confirm.
-					</label>
-					<input
-						name='confirmation'
-						id='confirmation'
-						type='text'
-						min='1'
-						max='20'
-						required
-						autoComplete='off'
-						disabled={loading || success === true}
-						pattern='superusers' />
-					{
-						loading === false && success !== true && (
-							<>
-								<button
-									type='button'
-									disabled={loading}
-									onClick={() => modalRef.current!.close()}>
-									Cancel
-								</button>
-								<button
-									disabled={loading}
-									type='submit'>
-									Confirm
-								</button>
-							</>
-						)
-					}
-					<div className={look.statusContainer}>
-						{error !== null && <p>{error}</p>}
-						{
-							success === true && (
-								<>
-									<p>Success! You will be signed out shortly.</p>
-									<button onClick={() => {
-										setSuccess(false);
-										setError(null);
-										setLoading(false);
-										modalRef.current!.close();
-									}}>Close</button>
-								</>
-							)
-						}
-					</div>
-					<p>status: {loading ? 'loading'
-						: success ? 'success'
-							: error !== null ? error
-								: 'idle'}</p>
-				</form>
-			</dialog>
-
-			<section className={look.container}>
-				<button
-					onClick={() => modalRef.current!.showModal()}
-					className={look.button}>
-					Delete account
-				</button>
-				<button
-					onClick={() => signOut()}
-					className={look.button}>
-					Sign out
-				</button>
-			</section >
-		</>
+		<section className={look.hidden}>
+			<DeleteDialog />
+			<button
+				onClick={() => signOut()}
+				className={look.button}>
+				Sign out
+			</button>
+		</section>
 	);
 }
 
