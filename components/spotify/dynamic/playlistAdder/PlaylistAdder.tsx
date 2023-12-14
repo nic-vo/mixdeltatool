c4bf66d48f13dbbcd9eee4c09f9ed9d7e60dd588
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-12-13 18:24:42 -0800

    Differentiate between local and global styles in the main Adder component

--- components/spotify/dynamic/playlistAdder/PlaylistAdder.tsx
+++ components/spotify/dynamic/playlistAdder/PlaylistAdder.tsx
@@ -3,7 +3,8 @@ import UserAdder from './userAdder/UserAdder';
 import SpecificAdder from './specificAdder/SpecificAdder';
 import PlaylistDisplay from './playlistDisplay/PlaylistDisplay';
 
-import look from './PlaylistAdder.module.scss';
+import local from './PlaylistAdder.module.scss';
+import global from '@styles/globals.module.scss';
 
 /*
 
@@ -18,12 +19,16 @@ export default function PlaylistAdder() {
 	const [onUser, setOnUser] = useState(true);
 
 	return (
-		<section className={look.container}>
+		<section className={local.container}>
 			<h1>Get some playlists for the tool</h1>
-			<section>
-				<button onClick={() => setOnUser(true)}>Your playlists</button>
-				<button onClick={() => setOnUser(false)}>Other playlists</button>
-			</section>
+			<div>
+				<button
+					onClick={() => setOnUser(true)}
+					className={global.emptyButton}>Add your playlists</button>
+				<button
+					onClick={() => setOnUser(false)}
+					className={global.emptyButton}>Add other playlists</button>
+			</div>
 			{
 				onUser ?
 					<UserAdder>
