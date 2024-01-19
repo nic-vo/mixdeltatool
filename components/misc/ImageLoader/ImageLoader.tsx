0655cefa235e8922a9ad6517ad2ef6512a6ccd5f
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-19 00:17:11 -0800

    Implement global image loader component

---
+++ components/misc/ImageLoader/ImageLoader.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { MixDeltaLogo } from '@consts/spotify';
+
+import local from './ImageLoader.module.scss';
+import LoadingLogo from '../LoadingLogo/LoadingLogo';
+
+const ImageLoader = (props: { url?: string | null | undefined, alt?: string }) => {
+	const [loaded, setLoaded] = useState(false);
+	const [error, setError] = useState(false);
+
+	const loadHandler = () => {
+		// setTimeout(() => setLoaded(true), 5000);
+		setLoaded(true);
+		setError(false);
+	}
+
+	const errorHandler = () => {
+		console.log('art error')
+		setError(true);
+	}
+
+	const containerClass = `${local.container}${!error
+		&& !loaded
+		&& props.url ? ` ${local.loading}` : ''}`;
+	const imgClass = `${local.img}${loaded ? ` ${local.loaded}` : ''}`;
+
+	if (props.url === null || props.url === undefined || error)
+		return (
+			<div className={containerClass}>
+				<img src={MixDeltaLogo.src}
+					alt={`Album art placeholder`}
+					className={imgClass} />
+			</div>
+		);
+
+	return (
+		<div className={containerClass}>
+			{!loaded && <LoadingLogo />}
+			<img
+				src={props.url !== null ? props.url : ''}
+				alt={props.alt ? props.alt : ''}
+				onLoad={loadHandler}
+				onError={errorHandler}
+				loading='lazy'
+				className={imgClass}
+			/>
+		</div>
+	);
+}
+
+export default ImageLoader;
