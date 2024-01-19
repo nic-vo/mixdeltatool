84cfe07939c2cadd73c6e6e18ed7b0ea9032a653
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-19 01:29:09 -0800

    Remove SpotifyEntry in favor of more of a layout

---
+++ components/spotify/SpotifyFrame.tsx
@@ -0,0 +1,48 @@
+import { signIn, useSession } from 'next-auth/react';
+import { SpotifyMain } from './dynamic';
+import { LoadingForDynamic, ServiceStatus } from '@components/misc';
+
+import global from '@styles/globals.module.scss';
+import { MixDeltaLogo } from '@consts/spotify';
+
+export default function SpotifyFrame(props: {
+	active: number,
+	status: string,
+	statusType: string
+}) {
+	const { status } = useSession();
+
+	// return <LoadingForDynamic />
+
+	if (status === 'loading') return <LoadingForDynamic />
+
+	if (status === 'authenticated') return <SpotifyMain />
+
+	return (
+		<>
+			<h1 style={{
+				textAlign: 'center',
+				width: '100%',
+				marginTop: '15svh'
+			}}>Sign in please!</h1>
+			<button
+				onClick={() => signIn()}
+				className={global.emptyButton}
+				style={{ margin: '5dvh' }}>Sign In</button>
+			<ServiceStatus {...props} />
+			<a
+				id='home'
+				href='/'
+				className={global.emptyButton}
+				style={{minWidth: '24ch', gap: '0.5em'}}>
+				<img
+					src={MixDeltaLogo.src}
+					alt='Click on this MixDelta logo to return to the homepage'
+					style={{
+						maxHeight: '64px',
+					}} />
+				Back to home
+			</a>
+		</>
+	);
+}
