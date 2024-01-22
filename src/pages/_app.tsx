986bd2f85f5559e27a1cddd5d9a46c653fdb5a37
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-22 01:38:25 -0800

    Add default components to _app
    Some head meta tags, footer

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,21 @@
 import type { AppProps } from 'next/app'
 
 import '@styles/globals.css';
+import Head from 'next/head';
+import { Footer } from '@components/misc';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
 	return (
-		<Component {...pageProps} />
+		<>
+			<Head>
+				<meta name='keywords' content='spotify playlist, spotify playlist comparison, spotify playlist tool, compare spotify playlist, spotify playlist, playlist cleanup' />
+				<meta charSet='UTF-8' />
+				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+			</Head>
+
+			<Component {...pageProps} />
+			<Footer />
+		</>
 	);
 };
