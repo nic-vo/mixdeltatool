cb99c401f3f5097ff289a89c0bdbeeb673a79e97
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-22 23:24:40 -0800

    Remove layout from SpotifyMain
    Now it's actually just provides the display component tree with all relevant contexts

--- components/spotify/dynamic/SpotifyMain.tsx
+++ components/spotify/dynamic/SpotifyMain.tsx
@@ -1,29 +1,20 @@
+import { DifferProvider } from './contexts/DifferProvider';
+import { GlobalLoadingProvider } from './contexts/GlobalLoadingProvider';
 import { SpecificPlaylistProvider } from './contexts/SpecificPlaylistProvider';
 import { UserPlaylistProvider } from './contexts/UserPlaylistProvider';
 import SpotifyRouter from './SpotifyRouter';
-import { signIn, useSession } from 'next-auth/react';
-
-import global from '@styles/globals.module.scss';
 
 const SpotifyMain = () => {
-	const { status } = useSession();
-
-	if (status === 'authenticated') return (
-		<UserPlaylistProvider>
-			<SpecificPlaylistProvider>
-				<SpotifyRouter />
-			</SpecificPlaylistProvider>
-		</UserPlaylistProvider>
-	)
 	return (
-		<>
-			<h1 style={{ textAlign: 'center', width: '100%' }}>Sign in please!</h1>
-			<div>
-				<button
-					onClick={() => signIn()}
-					className={global.emptyButton}>Sign in</button>
-			</div>
-		</>
+		<GlobalLoadingProvider>
+			<UserPlaylistProvider>
+				<SpecificPlaylistProvider>
+					<DifferProvider>
+						<SpotifyRouter />
+					</DifferProvider>
+				</SpecificPlaylistProvider>
+			</UserPlaylistProvider>
+		</GlobalLoadingProvider>
 	);
 }
 
