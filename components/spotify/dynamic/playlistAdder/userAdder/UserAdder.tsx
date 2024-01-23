ecd779282673fabfa03dc856c9afc629db0f27a4
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-22 23:35:17 -0800

    Reduce UserAdder to a control component
    Deals purely with the actions from UserProvider; no longer displays playlists
    Uses SmallStatus to display the state of fetching from UserProvider

--- components/spotify/dynamic/playlistAdder/userAdder/UserAdder.tsx
+++ components/spotify/dynamic/playlistAdder/userAdder/UserAdder.tsx
@@ -1,39 +1,32 @@
 import { useContext } from 'react';
 import { UserPlaylistContext } from '../../contexts/UserPlaylistProvider';
+import { SmallStatus } from '@components/misc';
+import { FaDownload } from 'react-icons/fa';
 
-import local from '../PlaylistAdder.module.scss';
+import local from './UserAdder.module.scss';
 import global from '@styles/globals.module.scss';
 
-export default function UserAdder(props: { children: React.ReactNode }) {
+export default function UserAdder() {
 	// Basically dump most of the context here because it requires every bit
 	const {
 		userCurrentPage,
 		userLoading,
 		userError,
-		getUserPlaylistsHandler,
-		clearUserPlaylistsHandler
+		getUserPlaylistsHandler
 	} = useContext(UserPlaylistContext);
+
+	const buttonClasser = `${global.emptyButton} ${local.button}${userCurrentPage === null ?
+		` ${local.done}` : ''}`
 
 	return (
 		<>
-			<section className={local.innerContainer}>
-				<button
-					onClick={getUserPlaylistsHandler}
-					disabled={userLoading || userCurrentPage === null}
-					className={global.emptyButton}>
-					{userCurrentPage === null ?
-						'No more playlists available' : 'Get more playlists'}
-				</button>
-				<button
-					onClick={clearUserPlaylistsHandler}
-					disabled={userLoading}
-					className={global.emptyButton}>
-					Clear your playlists</button>
-				<p>Next page {userCurrentPage !== null ? userCurrentPage : 'end'}</p>
-				<p>User {userLoading ? 'loading' : 'idle'}</p>
-				<p>{userError !== null && userError}</p>
-			</section>
-			{props.children}
+			<button
+				onClick={getUserPlaylistsHandler}
+				disabled={userLoading || userCurrentPage === null}
+				className={buttonClasser}>
+				<FaDownload /> {userCurrentPage === null ? 'No more!' : 'Retrieve'}
+			</button>
+			<SmallStatus error={userError} loading={userLoading} />
 		</ >
 	);
 };
