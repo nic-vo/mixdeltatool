a4290ca1cbd9b56e34e9112904b8aaeeabcc5ef6
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-22 19:07:58 -0800

    Misc commit: import and export components

--- components/misc/index.js
+++ components/misc/index.js
@@ -1,9 +1,21 @@
 import Background from './Background/Background';
 import Canvas from './Canvas/Canvas';
-import Loading from './Loading/Loading';
+import LoadingForDynamic from './LoadingForDynamic/LoadingForDynamic';
+import SmallStatus from './SmallStatus/SmallStatus';
+import ServiceStatus from './ServiceStatus/ServiceStatus';
+import ListItem from './listItem/ListItem';
+import LogoToAnimate from './LogoToAnimate/LogoToAnimate';
+import LoadingLogo from './LoadingLogo/LoadingLogo';
+import Footer from './Footer/Footer';
 
 export {
 	Background,
 	Canvas,
-	Loading
+	LoadingForDynamic,
+	SmallStatus,
+	ServiceStatus,
+	ListItem,
+	LogoToAnimate,
+	LoadingLogo,
+	Footer
 };
