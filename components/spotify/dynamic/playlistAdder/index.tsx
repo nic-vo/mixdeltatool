4e2e2c0fc522289ec798d1416b25078cd8c3b7ae
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-22 23:37:50 -0800

    Remove dynamic from playlistAdder
    It'll be first to load; save dynamic for Differ

--- components/spotify/dynamic/playlistAdder/index.tsx
+++ components/spotify/dynamic/playlistAdder/index.tsx
@@ -1,16 +1,3 @@
-import { Loading } from '@components/misc';
-import dynamic from 'next/dynamic';
-
-const PlaylistAdder = dynamic(import('./PlaylistAdder'), {
-	loading: (props: {
-		error?: Error | null,
-		pastDelay?: boolean,
-		timedOut?: boolean
-	}) => <Loading
-			error={props.error}
-			pastDelay={props.pastDelay}
-			timedOut={props.timedOut} />,
-	ssr: false
-});
+import PlaylistAdder from './PlaylistAdder';
 
 export default PlaylistAdder;
