bfcb0d1008ec5c97b5404f7e9940ccca18f5db81
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-22 22:44:32 -0800

    Adjust parsers to return values aligned with Spot dev agreement
    Strip unnecesssary keys

--- lib/spotify/validators.ts
+++ lib/spotify/validators.ts
@@ -9,34 +9,16 @@ const pageQueryParser = z.object({
 const idParamParser = z.string().length(22).regex(/^[A-Za-z0-9]{22}$/);
 const typeParamParser = z.enum(['playlist', 'album']);
 
-const specificQueryParser = z.object({
-	id: idParamParser,
-	type: typeParamParser
-}).strict();
-
-const playlistIdTypeParser = z.object({
-	id: idParamParser,
-	type: typeParamParser,
-	name: z.string(),
-	owner: z.string()
-}).strict();
-
-const diffBodyParser = z.object({
-	target: playlistIdTypeParser,
-	differ: playlistIdTypeParser,
-	type: z.enum(['adu', 'odu', 'otu', 'bu', 'stu'])
-}).strict();
-
 const basicSpotObjectParser = z.object({
 	id: z.string(),
 	uri: z.string(),
 	href: z.string()
 });
 
-const basicSpotAPIResponseParser = z.object({
-	href: z.string().url(),
-	next: z.nullable(z.string().url())
-});
+const specificQueryParser = z.object({
+	id: idParamParser,
+	type: typeParamParser
+}).strict();
 
 const spotUserObjectParser = basicSpotObjectParser.extend({
 	display_name: z.nullable(z.string())
@@ -46,34 +28,46 @@ const spotArtistObjectParser = basicSpotObjectParser.extend({
 	name: z.string()
 });
 
+const playlistIdTypeParser = z.object({
+	id: idParamParser,
+	type: typeParamParser,
+	name: z.string(),
+	owner: z.array(spotArtistObjectParser.pick({ id: true, name: true })),
+	tracks: z.number().int()
+}).strict();
+
+const diffBodyParser = z.object({
+	target: playlistIdTypeParser,
+	differ: playlistIdTypeParser,
+	type: z.enum(['adu', 'odu', 'otu', 'bu', 'stu'])
+}).strict();
+
+const basicSpotAPIResponseParser = z.object({
+	href: z.string().url(),
+	next: z.nullable(z.string().url())
+});
+
 const spotImageObjectParser = z.object({
 	url: z.string().url(),
-	height: z.nullable(z.number().int()),
-	width: z.nullable(z.number().int())
+	height: z.nullable(z.number().int()).optional(),
+	width: z.nullable(z.number().int()).optional()
 });
 
 const spotPlaylistObjectParser = basicSpotObjectParser.extend({
-	collaborative: z.boolean(),
-	description: z.nullable(z.string()),
 	images: z.array(spotImageObjectParser),
 	name: z.string(),
 	owner: spotUserObjectParser,
-	public: z.boolean(),
-	snapshot_id: z.string(),
 	tracks: z.object({ href: z.string().url(), total: z.number().int() }),
 	type: z.literal('playlist')
 });
 
 const spotAlbumObjectParser = basicSpotObjectParser.extend({
-	album_type: z.enum(['album', 'single', 'compilation']),
 	artists: z.array(spotArtistObjectParser),
 	total_tracks: z.number().int(),
-	available_markets: z.array(z.string()),
 	external_urls: z.object({ spotify: z.string() }),
 	images: z.array(spotImageObjectParser),
 	name: z.string(),
 	release_date: z.string(),
-	release_date_precision: z.enum(['year', 'month', ' day']),
 	tracks: z.object({ href: z.string().url() }),
 	type: z.literal('album')
 });
