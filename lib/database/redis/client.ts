77f3e3a51fa5e34c14743ba15fc1b6b848b865ed
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-23 15:37:11 -0800

    Change Redis creds to closer to prod server

--- lib/database/redis/client.ts
+++ lib/database/redis/client.ts
@@ -1,8 +1,8 @@
 import { CustomError } from '@lib/errors';
 import { Redis } from '@upstash/redis'
 
-const url = process.env.REDIS_WEST_URL;
-const token = process.env.REDIS_WEST_TOKEN;
+const url = process.env.REDIS_EAST_URL;
+const token = process.env.REDIS_EAST_TOKEN;
 
 if (!url || !token)
 	throw new CustomError(500, 'There was an error authenticating your request');
