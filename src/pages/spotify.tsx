96cc3a00d2e47c10a9996e86a2b4fb1847e44a86
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-22 21:02:43 -0800

    Turn Spotify page into more of a layout
    Accepts global status props
    Consts for <head>

--- src/pages/spotify.tsx
+++ src/pages/spotify.tsx
@@ -1,10 +1,46 @@
 import AuthProvider from '@components/auth/provider';
-import { SpotifyEntry } from '@components/spotify';
+import { SpotEUA } from '@components/legal';
+import { Background } from '@components/misc';
+import { Header, SpotifyFrame } from '@components/spotify';
+import { MAIN_DESC, MAIN_TITLE } from '@consts/spotify';
+import { GlobalStatusUpdater } from '@lib/misc';
+import Head from 'next/head';
 
-export default function Spotify() {
+export default function Spotify(props: {
+	status: string,
+	statusType: string,
+	active: number
+}) {
 	return (
-		<AuthProvider>
-			<SpotifyEntry />
-		</AuthProvider>
+		<>
+			<Head>
+				<title>{MAIN_TITLE}</title>
+				<meta name='description' content={MAIN_DESC} />
+			</Head>
+
+			<Header />
+			<main style={{
+				position: 'relative',
+				display: 'flex',
+				flexDirection: 'column',
+				alignItems: 'center',
+				gap: '2svh',
+				width: '80svw',
+				height: '100svh',
+				margin: 'auto',
+				zIndex: 1,
+			}}>
+				<SpotEUA />
+				<AuthProvider>
+					<SpotifyFrame {...props} />
+				</AuthProvider>
+			</main>
+			<Background />
+		</>
 	);
+}
+
+export async function getStaticProps() {
+	const globalStatus = await GlobalStatusUpdater();
+	return { props: { ...globalStatus } };
 }
