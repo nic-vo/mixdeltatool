099fc8f529dd30372f08a6729bcb8dabf5e3eabb
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-22 22:16:35 -0800

    Improve home page
    Add global status indicator; better copy; <head> elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,45 @@
-import { SpotEUA } from '@components/legal';
-import { Background } from '@components/misc';
+import { Background, ServiceStatus } from '@components/misc';
+import { GlobalStatusUpdater } from '@lib/misc';
+import Head from 'next/head';
+import { FaArrowCircleRight } from 'react-icons/fa';
+
+import { APP_NAME, MAIN_DESC, MAIN_TITLE } from '@consts/spotify';
 
 import local from '@styles/Home.module.scss';
+import global from '@styles/globals.module.scss';
 
-export default function Home() {
+export default function Home(props: {
+	status: string,
+	statusType: string,
+	active: number
+}) {
 	return (
-		<main className={local.main}>
-			<h1>Homepage</h1>
-			<section>
-				<h2>What this is</h2>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tempus urna et pharetra pharetra massa. Blandit cursus risus at ultrices mi tempus. Feugiat in ante metus dictum at tempor. Tincidunt augue interdum velit euismod in pellentesque. Eu turpis egestas pretium aenean pharetra magna ac. Curabitur vitae nunc sed velit dignissim sodales ut. Neque sodales ut etiam sit amet nisl purus in mollis. Feugiat pretium nibh ipsum consequat nisl vel pretium lectus quam. Hendrerit dolor magna eget est. Nulla aliquet enim tortor at auctor. Mauris pharetra et ultrices neque. Tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Urna molestie at elementum eu facilisis sed odio morbi quis. Leo integer malesuada nunc vel risus commodo viverra maecenas.
-				</p>
-			</section>
-			<section>
-				<h2>Who's it for</h2>
-				<p>Praesent tristique magna sit amet. Euismod quis viverra nibh cras pulvinar mattis nunc. Eu augue ut lectus arcu bibendum at varius vel pharetra. Ut sem viverra aliquet eget sit amet. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Nec nam aliquam sem et tortor consequat id porta nibh. Volutpat ac tincidunt vitae semper. Arcu non odio euismod lacinia at quis. Elementum curabitur vitae nunc sed. Amet commodo nulla facilisi nullam vehicula. Facilisi cras fermentum odio eu feugiat pretium nibh ipsum. Scelerisque eleifend donec pretium vulputate sapien nec sagittis aliquam. Risus quis varius quam quisque id diam vel. Sem nulla pharetra diam sit. Tortor vitae purus faucibus ornare suspendisse sed nisi. Lacus luctus accumsan tortor posuere ac ut consequat. Turpis tincidunt id aliquet risus feugiat in. Lobortis mattis aliquam faucibus purus in massa tempor nec feugiat. Consequat mauris nunc congue nisi vitae suscipit tellus mauris.</p>
-			</section>
-			<br /><br /><br />
-			<a href='/spotify'>Here's a link to the tool</a>
-			<a href='/privacypolicy'>Here's a link to the privacy policy</a>
-			<SpotEUA />
+		<>
+			<Head>
+				<title>{MAIN_TITLE}</title>
+				<meta name='description' content={MAIN_DESC} />
+			</Head>
+
+			<main className={local.main}>
+				<h1 className={local.heading}>Welcome to the {APP_NAME} tool for Spotify!</h1>
+				<a href='/spotify' className={global.emptyButton.concat(' ', local.toolLink)}>To the tool! <FaArrowCircleRight /></a>
+				<ServiceStatus {...props} />
+				<section className={local.description}>
+					<h2>About this tool</h2>
+					<section className={local.sectionLeft}>
+						<p>Welcome to MixDelta, your ultimate companion for mastering your Spotify playlists with ease. Designed for playlist aficionados who curate an extensive library, MixDelta empowers you to efficiently compare and manage your playlists like never before. Say goodbye to the hassle of manually syncing changes across multiple playlists – MixDelta does the heavy lifting for you.</p>
+					</section>
+					<section className={local.sectionRight}>
+						<p>With MixDelta, you can seamlessly compare two Spotify playlists, identifying shared tracks and unique gems in each. Tailor your new playlists based on your preferences – whether you want a compilation of shared tracks, exclusive additions from each playlist, or any combination in between. The power is in your hands.</p>
+					</section>
+				</section>
+			</main>
 			<Background />
-		</main>
+		</>
 	);
+}
+
+export async function getStaticProps() {
+	const globalStatus = await GlobalStatusUpdater();
+	return { props: { ...globalStatus } };
 }
