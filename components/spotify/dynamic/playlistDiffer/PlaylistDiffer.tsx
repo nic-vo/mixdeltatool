a9a96e129bc68bfdb93c30d4b5f942edf2312da3
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-22 23:40:39 -0800

    Differ now consumes Differ Context
    Persists diffing state even if user switches back to Adder
    Add PendingWindow based on state of diff

--- components/spotify/dynamic/playlistDiffer/PlaylistDiffer.tsx
+++ components/spotify/dynamic/playlistDiffer/PlaylistDiffer.tsx
@@ -1,10 +1,23 @@
-import DifferForm from "./differForm/DifferForm";
-import PlaylistOptions from "./playlistOptions/PlaylistOptions";
+import DifferForm from './differForm/DifferForm';
+import PlaylistOptions from './playlistOptions/PlaylistOptions';
+import { useContext } from 'react';
+import PendingWindow from './pendingWindow/PendingWindow';
+import { DifferContext } from '../contexts/DifferProvider';
+
+import local from './PlaylistDiffer.module.scss';
 
 export default function PlaylistDiffer() {
+	const { onForm } = useContext(DifferContext)
+
 	return (
-		<DifferForm>
-			<PlaylistOptions />
-		</DifferForm>
+		<section className={local.container}>
+			{
+				onForm ? (
+					<DifferForm>
+						<PlaylistOptions />
+					</DifferForm>)
+					: <PendingWindow />
+			}
+		</section>
 	);
 }
