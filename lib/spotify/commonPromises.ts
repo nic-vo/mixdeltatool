66cf4ef59dcfaf4e870605bc5a1b6eb2f2f53278
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-01-31 04:00:00 -0800

    Add boilerplate reducing promise
    Replace userGetter boilerplate with it

--- lib/spotify/commonPromises.ts
+++ lib/spotify/commonPromises.ts
@@ -3,17 +3,22 @@ import {
 	AuthError,
 	FetchError,
 	ForbiddenError,
+	MalformedError,
+	NotFoundError,
 	RateError,
 	TimeoutError
 } from '../errors';
 import { spotUserObjectParser } from './validators';
 
 export async function localTimeout<D>(
-	globalTimeoutMS: number,
-	localLimit: number
+	parentTimeout: number,
 ): Promise<D> {
+	/*
+		In parent, calculate the value by subtracting how much time to leave for
+		Rest of execution from globalTimeoutMS
+	*/
 	return new Promise(async (_, rej) => {
-		await new Promise(async r => setTimeout(() => r, Date.now() - globalTimeoutMS - localLimit));
+		await new Promise(async r => setTimeout(() => r, parentTimeout - Date.now()));
 		return rej(new TimeoutError());
 	})
 };
@@ -23,56 +28,22 @@ export async function userGetter(args: {
 	globalTimeoutMS: number
 }): Promise<string> {
 	const { accessToken, globalTimeoutMS } = args;
-	const localLimit = 2200;
-	const localTimeoutMS = globalTimeoutMS - localLimit;
+	const parentTimeoutMS = globalTimeoutMS - 5000;
 	const url = SPOT_URL_BASE.concat('me');
 	const headers = new Headers();
 	headers.append('Authorization', `Bearer ${accessToken}`);
 
 	return Promise.race([
-		localTimeout<string>(globalTimeoutMS, localLimit),
+		localTimeout<string>(parentTimeoutMS),
 		new Promise<string>(async (res, rej) => {
 			try {
-				let response;
-				let networkRetry = true;
-				while (true) {
-					try {
-						response = await fetch(url, { headers });
-					} catch {
-						if (networkRetry === false)
-							throw new FetchError('There was an error reaching Spotify');
-						networkRetry = false;
-						continue;
-					}
-					networkRetry = true;
-					if (response.status === 429) {
-						const tryHeader = response.headers.get('Retry-After');
-						// Retry based on Retry-After header in sec, otherwise 2s wait
-						const retry = tryHeader !== null ? parseInt(tryHeader) * 1000 : 2000;
-						// Throw rate error if wait would pass the timeout time
-						if ((Date.now() + retry) >= localTimeoutMS) throw new RateError(10);
-						// Await retry if within timeout time
-						await new Promise(async r => setTimeout(r, retry));
-						// Continue for while loop
-						continue;
-					}
-					break;
-				}
-				if (!response)
-					throw new FetchError('There was an error reaching Spotify');
-				if (response.ok === false) {
-					// This is if somehow after all this, Spotify detects something wrong
-					switch (response.status) {
-						case 401:
-							throw new AuthError();
-						case 403:
-							throw new ForbiddenError(`We can't find you`);
-						default:
-							throw new FetchError('There was an error reaching Spotify');
-					}
-				}
-
-				let id;
+				const request = fetch(url, { headers });
+				const response = await getOnePromise({
+					request,
+					silentFail: false,
+					parentTimeoutMS
+				})
+				let id: string;
 				try {
 					const jsoned = await response.json();
 					const parsed = spotUserObjectParser.parse(jsoned);
@@ -80,9 +51,66 @@ export async function userGetter(args: {
 				} catch {
 					throw new FetchError("There was an error with Spotify's response");
 				}
-				return res(id as string);
+				return res(id);
 			} catch (e: any) {
 				return rej(e);
 			}
 		})]);
+}
+
+export async function getOnePromise(params:
+	{
+		request: Promise<Response>,
+		parentTimeoutMS: number,
+		silentFail: boolean,
+		errorOverrides?: { status: number, message: string }[]
+	}
+): Promise<Response> {
+	const { request, parentTimeoutMS, silentFail, errorOverrides } = params;
+	return new Promise(async (res, rej) => {
+		try {
+			let response: Response;
+			let networkRetry = true;
+			while (true) {
+				try {
+					response = await request;
+				} catch {
+					if (networkRetry === false)
+						throw new FetchError();
+					networkRetry = false;
+					continue;
+				}
+				networkRetry = true;
+				if (response.status === 429) {
+					const tryHeader = response.headers.get('Retry-After');
+					// Retry based on Retry-After header in sec, otherwise 2s wait
+					const retry = tryHeader !== null ? parseInt(tryHeader) * 1000 : 2000;
+					// Throw rate error if wait would pass the timeout time
+					if ((Date.now() + retry) >= parentTimeoutMS)
+						throw new RateError(retry / 1000);
+					// Await retry if within timeout time
+					await new Promise(async r => setTimeout(r, retry));
+					// Continue for while loop
+					continue;
+				}
+				break;
+			}
+			if (response.ok) res(response);
+			// If !ok and silentFail, the whole fetch doesn't matter
+			if (silentFail) res(response);
+			if (errorOverrides)
+				for (const custom of errorOverrides)
+					if (response.status === custom.status)
+						throw custom;
+			switch (response.status) {
+				case 400: throw new MalformedError();
+				case 401: throw new AuthError();
+				case 403: throw new ForbiddenError();
+				case 404: throw new NotFoundError();
+				default: throw new FetchError("There's something wrong with Spotify");
+			}
+		} catch (e: any) {
+			return rej(e);
+		}
+	});
 }
