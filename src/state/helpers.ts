b983ba76c10deff4d7d69ef97e150fd7646e5825
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-02-01 13:28:39 -0800

    Move / init playlist persistence stuff to helpers.ts

---
+++ src/state/helpers.ts
@@ -0,0 +1,61 @@
+import { myPlaylistObjectParser } from '@lib/spotify/validators';
+import { sanitize } from 'isomorphic-dompurify';
+
+import { MyPlaylistObject } from '@components/spotify/types';
+
+export const sanitizePlaylists = (playlists: MyPlaylistObject[]) => {
+	try {
+		const parsed = playlists.map(playlist =>
+			myPlaylistObjectParser.parse(playlist)
+		);
+		const sanitized = parsed.map(playlist => {
+			return {
+				...playlist,
+				name: sanitize(playlist.name),
+				id: sanitize(playlist.id),
+				image: !playlist.image ? undefined :
+					{
+						...playlist.image,
+						url: sanitize(playlist.image.url)
+					}
+			};
+		});
+		return sanitized;
+	} catch {
+		return [];
+	}
+}
+
+export const initPlaylists = (key: string): MyPlaylistObject[] => {
+	const storageData = sessionStorage.getItem(key);
+	if (storageData === null) return [];
+
+	try {
+		const parsed = JSON.parse(storageData) as MyPlaylistObject[];
+		const sanitized = sanitizePlaylists(parsed);
+		return sanitized;
+	} catch { }
+	return [];
+}
+
+export const initPage = (key: string): number | null => {
+	const storage = sessionStorage.getItem(key);
+	if (storage === null) return 0;
+	if (storage === 'null') return null;
+	try {
+		const parsed = parseInt(storage);
+		return parsed >= 0 && parsed < 50 ? parsed : 0;
+	} catch { }
+	return 0;
+}
+
+export const persistPlaylists = (
+	playlists: MyPlaylistObject[],
+	key: string
+) => {
+	sessionStorage.setItem(key, JSON.stringify(playlists));
+}
+
+export const persistPage = (page: number | null, key: string) => {
+	sessionStorage.setItem(key, page === null ? 'null' : page.toString());
+}
