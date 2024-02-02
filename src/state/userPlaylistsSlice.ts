4f625c3b2422b367c5e9d3fbe5372def93e4f88b
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-02-01 16:15:30 -0800

    Flesh out userPlaylistSlice
    Replace loading state with loadStates reducers
    Import and use the init and persist helpers
    Cleanup the async logic
    Export the reducer for the differ

--- src/state/userPlaylistsSlice.ts
+++ src/state/userPlaylistsSlice.ts
@@ -4,48 +4,90 @@ import {
 	createSlice
 } from '@reduxjs/toolkit';
 import { signIn } from 'next-auth/react';
+import {
+	initPage,
+	initPlaylists,
+	persistPage,
+	persistPlaylists
+} from './helpers';
+import { endUserLoading, startUserLoading } from './loadStatesSlice';
 
-import type { MyPlaylistObject } from '@components/spotify/types';
+import type {
+	MyPlaylistObject,
+	MyUserAPIRouteResponse
+} from '@components/spotify/types';
 
 type userPage = number | null;
 type userError = string | null;
+
+const PLAYLISTS_KEY = 'USER_PLAYLISTS';
+const PAGE_KEY = 'USER_PLAYLISTS_PAGE';
 
 const initialState: {
 	playlists: MyPlaylistObject[],
 	page: userPage,
 	error: userError,
-	loading: boolean
 } = {
-	playlists: [],
-	page: 0,
+	playlists: initPlaylists(PLAYLISTS_KEY),
+	page: initPage(PAGE_KEY),
 	error: null,
-	loading: false,
 };
 
 const userPlaylistsSlice = createSlice({
 	name: 'userPlaylists',
 	initialState,
-	reducers: {},
+	reducers: {
+		postDiffAddition: (state, action: PayloadAction<MyPlaylistObject>) => {
+			state.playlists = [...state.playlists, action.payload];
+		}
+	},
 	extraReducers: (builder) => {
-		builder.addCase(thingy.pending, (state) => {
-			state.loading = true;
+		builder.addCase(retrieveUserListsAsync.pending, (state) => {
+			startUserLoading();
 			state.error = null;
-		})
-			.addCase(thingy.fulfilled, (state) => {
-				state.loading = false;
-			});
+		}).addCase(retrieveUserListsAsync.fulfilled,
+			(state, action: PayloadAction<MyUserAPIRouteResponse>) => {
+				endUserLoading();
+				state.page = action.payload.next;
+				const setted = Array.from(
+					new Set([...state.playlists, ...action.payload.playlists])
+				);
+				state.playlists = setted;
+				persistPlaylists(setted, PLAYLISTS_KEY);
+				persistPage(action.payload.next, PAGE_KEY);
+			}).addCase(retrieveUserListsAsync.rejected, (state, action) => {
+				endUserLoading();
+				state.error = typeof (action.error.message) === 'string' ?
+					action.error.message : 'Unknown error';
+			})
 	}
 });
 
-export const thingy = createAsyncThunk(
-	'userPlaylists/thingy',
-	async () => {
-		try {
-			const response = await fetch('/api/spotify/getUser')
-		} catch (e:any) {
+export const selectUserPlaylists = (s: typeof initialState) => s.playlists;
 
+export const { postDiffAddition } = userPlaylistsSlice.actions;
+
+export const retrieveUserListsAsync = createAsyncThunk(
+	'userPlaylists/retrieveUserListsAsync',
+	async (page: number) => {
+		try {
+			let response;
+			try {
+				response = await fetch(`/api/spotify/getUser?page=${page}`);
+			} catch {
+				throw { message: 'There was an error reaching our servers' };
+			}
+			const jsoned = await response.json();
+			// Return early if okay
+			if (response.ok) return jsoned as MyUserAPIRouteResponse;
+			// Any errors get thrown as expected {message: string}
+			if (response.status === 401) signIn();
+			throw jsoned as { message: string };
+		} catch (e: any) {
+			// In case I missed something
+			throw e.message ? e.message : { message: 'Bad response from server' };
 		}
 	}
-)
+);
 
 export default userPlaylistsSlice.reducer;
