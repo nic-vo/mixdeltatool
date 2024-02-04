5a843ba066831992099944bb56986abeb1d51ab4
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-02-03 19:38:08 -0800

    Adapt clear button to redux

--- src/components/spotify/dynamic/playlistAdder/ClearPlaylistButton/ClearPlaylistButton.tsx
+++ src/components/spotify/dynamic/playlistAdder/ClearPlaylistButton/ClearPlaylistButton.tsx
@@ -1,28 +1,31 @@
-import { useContext } from 'react'
-import { SpecificPlaylistContext } from '../../contexts/SpecificPlaylistProvider'
-import { UserPlaylistContext } from '../../contexts/UserPlaylistProvider'
-import { GlobalLoadingContext } from '../../contexts/GlobalLoadingProvider';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectSpecificFetch, selectUserFetch } from '@state/state';
+import { clearUser } from '@state/userPlaylistsSlice';
+import { clearSpecific } from '@state/specificPlaylistsSlice';
 import { FaTimes } from 'react-icons/fa';
 
 import local from './ClearPlaylistButton.module.scss';
 import global from '@styles/globals.module.scss';
 
 const ClearPlaylistButton = (props: { user: boolean }) => {
-	const { clearSpecificPlaylistsHandler, specificLoading } = useContext(SpecificPlaylistContext);
-	const { clearUserPlaylistsHandler, userLoading } = useContext(UserPlaylistContext);
-	const { gLoading } = useContext(GlobalLoadingContext);
+	const dispatch = useDispatch();
 
-	let memoizedHandler = props.user ?
-		clearUserPlaylistsHandler : clearSpecificPlaylistsHandler;
+	const { loading: userLoading } = useSelector(selectUserFetch);
+	const { loading: specificLoading } = useSelector(selectSpecificFetch);
+
+	let memoizedHandler = () => {
+		props.user ?
+			dispatch(clearUser()) : dispatch(clearSpecific());
+	}
 
 	const classer = global.emptyButton.concat(' ', local.button);
 
 	return (
 		<button
-			disabled={gLoading || (props.user ? userLoading : specificLoading)}
+			disabled={props.user ? userLoading : specificLoading}
 			onClick={memoizedHandler}
-			className={classer}>
-			<FaTimes /> Clear Playlists</button>
+			className={classer} >
+			<FaTimes /> Clear Playlists</button >
 	);
 }
 
