769a5b9a5fa7f300bb9ec2e46a825516aa09d268
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-02-03 19:47:03 -0800

    Adapt PlaylistOptions to redux
    Remove weird useMemo that doesn't need to work now that redux is sliced

--- src/components/spotify/dynamic/playlistDiffer/playlistOptions/PlaylistOptions.tsx
+++ src/components/spotify/dynamic/playlistDiffer/playlistOptions/PlaylistOptions.tsx
@@ -1,18 +1,31 @@
-import { useContext, useMemo } from 'react';
-import { UserPlaylistContext } from '../../contexts/UserPlaylistProvider';
-import { SpecificPlaylistContext } from '../../contexts/SpecificPlaylistProvider';
+import { useSelector } from 'react-redux';
+import { selectSpecificPlaylists, selectUserPlaylists } from '@state/state';
 
 export default function PlaylistOptions() {
-	const { userPlaylists } = useContext(UserPlaylistContext);
-	const { specificPlaylists } = useContext(SpecificPlaylistContext);
-	const memoized = useMemo(() => {
-		return (
-			<>
-				<option value=''>Choose one...</option>
-				{
-					specificPlaylists.length > 0 &&
-					<optgroup label='Specific Playlists'>
-						{specificPlaylists.map((playlist) => {
+	const userPlaylists = useSelector(selectUserPlaylists);
+	const specificPlaylists = useSelector(selectSpecificPlaylists);
+
+	return (
+		<>
+			<option value=''>Choose one...</option>
+			{
+				specificPlaylists.length > 0 &&
+				<optgroup label='Specific Playlists'>
+					{specificPlaylists.map((playlist) => {
+						const { id, name, owner, tracks } = playlist;
+						return (
+							<option value={id} key={id}>
+								{name} / {owner.map((user, index) => `${user.name}${index < owner.length - 1 ? ',' : ''}`)} / {tracks} tracks
+							</option>
+						)
+					})}
+				</optgroup>
+			}
+			{
+				userPlaylists.length > 0 &&
+				<optgroup label='Your Playlists'>
+					{
+						userPlaylists.map((playlist) => {
 							const { id, name, owner, tracks } = playlist;
 							return (
 								<option value={id} key={id}>
@@ -20,24 +33,8 @@ export default function PlaylistOptions() {
 								</option>
 							)
 						})}
-					</optgroup>
-				}
-				{
-					userPlaylists.length > 0 &&
-					<optgroup label='Your Playlists'>
-						{
-							userPlaylists.map((playlist) => {
-								const { id, name, owner, tracks } = playlist;
-								return (
-									<option value={id} key={id}>
-										{name} / {owner.map((user, index) => `${user.name}${index < owner.length - 1 ? ',' : ''}`)} / {tracks} tracks
-									</option>
-								)
-							})}
-					</optgroup>
-				}
-			</>
-		)
-	}, [userPlaylists, specificPlaylists]);
-	return memoized;
+				</optgroup>
+			}
+		</>
+	)
 };
