af3b05f2590b02cc1f1b6fba6906fa96ba986217
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-01 23:33:42 -0700

    init tailwind and add font var names

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				overpass: '--font-overpass',
+				notosans: '--font-notosans',
+			},
+		},
+	},
+	plugins: [],
+};
