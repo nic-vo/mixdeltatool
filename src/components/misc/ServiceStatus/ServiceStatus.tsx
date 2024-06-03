be4d8a1ac0240bafc965da6940db60a1e97ecce0
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-03 15:46:39 -0700

    Move server status fetch to within component
    Tailwind-ify init
    fa to io5

--- src/components/misc/ServiceStatus/ServiceStatus.tsx
+++ src/components/misc/ServiceStatus/ServiceStatus.tsx
@@ -1,35 +1,44 @@
 import {
-	FaExclamationTriangle,
-	FaExclamationCircle,
-	FaCheckCircle
-} from 'react-icons/fa';
+	IoWarningSharp,
+	IoAlertCircleSharp,
+	IoCheckmarkCircle,
+} from 'react-icons/io5';
+import { getGlobalStatusProps } from '@/lib/database/mongoose';
 
 import local from './ServiceStatus.module.scss';
 
-const ServiceStatus = (props: {
-	status: string,
-	statusType: string,
-	active: number
-}) => {
-	const { status, statusType, active } = props;
+const AlertSVG = ({ statusType }: { statusType: string }) => {
+	if (statusType === 'severe') return <IoWarningSharp aria-hidden={true} />;
+	if (statusType === 'ok') return <IoCheckmarkCircle aria-hidden={true} />;
+	return <IoAlertCircleSharp aria-hidden={true} />;
+};
+const text = '';
+export default async function ServiceStatus() {
+	const { status, statusType, active } = await getGlobalStatusProps();
 	const current = new Date(Math.floor(active / 60000) * 60000);
 
-	const divClasser = `${local.container} ${statusType === 'severe' ? local.severe
-		: statusType === 'ok' ? local.ok : local.concern}`;
+	const divClasser = `${local.container} ${
+		statusType === 'severe'
+			? local.severe
+			: statusType === 'ok'
+			? local.ok
+			: local.concern
+	}`;
 
 	return (
-		<section className={divClasser}>
-			{
-				statusType === 'severe' ? <FaExclamationTriangle /> :
-					statusType === 'ok' ? <FaCheckCircle /> : <FaExclamationCircle />
-			}
-
+		<div
+			className={`w-full max-w-prose flex items-center self-center gap-4 p-4 rounded-xl border-2`}
+			role={statusType === 'ok' ? 'none' : 'status'}>
+			<AlertSVG statusType={statusType} />
 			<div className={local.info}>
-				<p><strong>{current.getMonth() + 1}/{current.getDate()} @ {current.toLocaleTimeString().replace(/:00 /, ' ')}</strong></p>
+				<p>
+					<strong>
+						{current.getMonth() + 1}/{current.getDate()} @{' '}
+						{current.toLocaleTimeString().replace(/:00 /, ' ')}
+					</strong>
+				</p>
 				<p>{status}</p>
 			</div>
-		</section>
+		</div>
 	);
 }
-
-export default ServiceStatus;
