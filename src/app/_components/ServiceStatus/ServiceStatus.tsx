c9061974199e5aeb2c917e69cdedce41523cf915
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-05 13:29:48 -0700

    Tailwindify ServiceStatus

--- src/app/_components/ServiceStatus/ServiceStatus.tsx
+++ src/app/_components/ServiceStatus/ServiceStatus.tsx
@@ -5,39 +5,52 @@ import {
 } from 'react-icons/io5';
 import { getGlobalStatusProps } from '@/lib/database/mongoose';
 
-import local from './ServiceStatus.module.scss';
-
-const AlertSVG = ({ statusType }: { statusType: string }) => {
-	if (statusType === 'severe') return <IoWarningSharp aria-hidden={true} />;
+const AlertSVG = ({
+	statusType,
+}: {
+	statusType: 'ok' | 'severe' | 'concern';
+}) => {
+	if (statusType === 'severe')
+		return (
+			<IoWarningSharp
+				aria-hidden={true}
+				className='text-red-700'
+			/>
+		);
 	if (statusType === 'ok') return <IoCheckmarkCircle aria-hidden={true} />;
-	return <IoAlertCircleSharp aria-hidden={true} />;
+	return (
+		<IoAlertCircleSharp
+			aria-hidden={true}
+			className='text-orange-500'
+		/>
+	);
 };
 
 export default async function ServiceStatus() {
 	const { status, statusType, active } = await getGlobalStatusProps();
 	const current = new Date(Math.floor(active / 60000) * 60000);
 
-	const divClasser = `${local.container} ${
-		statusType === 'severe'
-			? local.severe
-			: statusType === 'ok'
-			? local.ok
-			: local.concern
-	}`;
-
 	return (
 		<div
-			className={`w-full max-w-prose flex items-center self-center gap-4 p-4 rounded-xl border-2`}
+			className={`w-full max-w-prose flex items-center self-center gap-4 p-4 rounded-xl border-2 ${
+				statusType === 'severe'
+					? 'border-red-700 bg-white'
+					: statusType === 'ok'
+					? 'bg-transparent border-white'
+					: 'border-orange-500 bg-white'
+			}`}
 			role={statusType === 'ok' ? 'none' : 'status'}>
 			<AlertSVG statusType={statusType} />
-			<div className={local.info}>
-				<p>
+			<div className='flex flex-col gap-2 last:*:border-t last:*:border-t-black pt-2'>
+				<p className={statusType === 'ok' ? 'text-white' : 'text-black'}>
 					<strong>
 						{current.getMonth() + 1}/{current.getDate()} @{' '}
 						{current.toLocaleTimeString().replace(/:00 /, ' ')}
 					</strong>
 				</p>
-				<p>{status}</p>
+				<p className={statusType === 'ok' ? 'text-white' : 'text-black'}>
+					{status}
+				</p>
 			</div>
 		</div>
 	);
