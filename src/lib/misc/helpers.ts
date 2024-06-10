5974368dce4f5585c8166deb1b6249b2d9e57e44
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-10 11:53:08 -0700

    Init two wrappers for route handlers
    One for timing out handlers w/auth and one w/o auth

--- src/lib/misc/helpers.ts
+++ src/lib/misc/helpers.ts
@@ -1,4 +1,9 @@
 import { randomBytes } from 'crypto';
+import { NextRequest } from 'next/server';
+import { TimeoutResponse } from '../returners';
+import { AppRouteHandlerFnContext } from 'next-auth/lib/types';
+import { NextAuthRequest } from 'next-auth/lib';
+import { auth } from '@/auth';
 
 export function printTime(message: string, start: number) {
 	const now = Date.now();
@@ -14,4 +19,43 @@ export function genUId(length: number) {
 		result += chars.charAt(buffer.readUInt8(i) % chars.length);
 	}
 	return result;
+}
+
+type BaseHandler = (
+	req: NextRequest,
+	ctx: AppRouteHandlerFnContext
+) => Promise<Response>;
+
+type HandlerAcceptingAuth = (
+	req: NextAuthRequest,
+	ctx: AppRouteHandlerFnContext
+) => Promise<Response>;
+
+export async function handlerWithTimeout(
+	timeoutS: number,
+	handler: BaseHandler
+) {
+	return async (req: NextRequest, ctx: AppRouteHandlerFnContext) =>
+		await Promise.race([
+			handler(req, ctx),
+			new Promise((res) =>
+				setTimeout(
+					() => res(TimeoutResponse()),
+					Math.floor(timeoutS - 2) * 1000
+				)
+			),
+		]);
+}
+
+export async function handlerWithTimeoutAndAuth(
+	timeoutS: number,
+	handler: HandlerAcceptingAuth
+) {
+	return async (req: NextAuthRequest, ctx: AppRouteHandlerFnContext) =>
+		await Promise.race([
+			auth(handler)(req, ctx),
+			new Promise<Response>((res) =>
+				setTimeout(() => res(TimeoutResponse()), (timeoutS - 1) * 1000)
+			),
+		]);
 }
