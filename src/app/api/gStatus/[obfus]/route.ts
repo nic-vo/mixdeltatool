185b3a08ce5e4d5c7485b0cc9189d45d775886f8
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-20 03:36:53 -0700

    Rewrite global status route
    Now uses global wrapper
    Reduced try/catch nesting
    Limit revaliation to relevant unstable_cache tag

--- src/app/api/gStatus/[obfus]/route.ts
+++ src/app/api/gStatus/[obfus]/route.ts
@@ -2,15 +2,9 @@ import { setNewGlobalStatus } from '@/lib/database/mongoose';
 import checkAndUpdateEntry from '@/lib/database/redis/ratelimiting';
 import { OPTIONS } from '../../_lib';
 import { handlerWithTimeout } from '@/lib/misc/helpers';
-import {
-	CreatedResponse,
-	MalformedResponse,
-	NotFoundResponse,
-	RateLimitResponse,
-	ServerErrorResponse,
-} from '@/lib/returners';
+import badResponse, { CreatedResponse } from '@/lib/returners';
 import { z } from 'zod';
-import { revalidatePath } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 
 import { NextRequest } from 'next/server';
 import { AppRouteHandlerFnContext } from 'next-auth/lib/types';
@@ -30,48 +24,36 @@ export const maxDuration = 20;
 export { OPTIONS };
 
 export const POST = handlerWithTimeout(
-	maxDuration,
+	{
+		maxDuration,
+		rateLimit: {
+			RATE_LIMIT_DECAY_SECONDS,
+			RATE_LIMIT_PREFIX,
+			RATE_LIMIT_ROLLING_LIMIT,
+		},
+	},
 	async (req: NextRequest, { params }: AppRouteHandlerFnContext) => {
-		if (process.env.GLOBAL_SAFETY === 'ON') return NotFoundResponse();
 		if (
 			!params ||
 			params['obfus'] !== process.env.NEXT_PUBLIC_GLOBAL_STATUS_UPDATE_ROUTE
 		)
-			return NotFoundResponse();
+			return badResponse(404);
 
 		let body;
 		try {
 			body = validator.parse(await req.json());
 		} catch {
-			return MalformedResponse();
+			return badResponse(400);
 		}
 		const { status, statusType, token } = body;
 		if (token !== process.env.NEXT_PUBLIC_GLOBAL_STATUS_UPDATE_SECRET)
-			return NotFoundResponse();
+			return badResponse(404);
 
-		try {
-			const { ip } = req;
-			if (!ip) return ServerErrorResponse('IP was missing somehow');
-			const rateLimit = await checkAndUpdateEntry({
-				ip,
-				prefix: RATE_LIMIT_PREFIX,
-				rollingLimit: RATE_LIMIT_ROLLING_LIMIT,
-				rollingDecaySeconds: RATE_LIMIT_DECAY_SECONDS,
-			});
-			if (rateLimit !== null) return RateLimitResponse(rateLimit);
-		} catch {
-			return ServerErrorResponse('Error rate limiting IP');
-		}
+		const dbCall = await setNewGlobalStatus({ status, statusType });
+		if (!dbCall.ok) return dbCall;
 
-		await setNewGlobalStatus({ status, statusType });
-		try {
-			revalidatePath('/');
-			revalidatePath('/spotify');
-		} catch {
-			return ServerErrorResponse(
-				'There was an error revalidating the cache for the paths'
-			);
-		}
+		revalidateTag('internalGlobalStatus');
+
 		return CreatedResponse('Status updated');
 	}
 );
