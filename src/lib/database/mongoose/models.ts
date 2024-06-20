256140cedb7260e431b51a1887fc79a97e4197aa
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-20 02:17:23 -0700

    Clean up mongoose models a bit
    Ignore the weird Next.js caching stuff for models for now
    Import the types from the right module for mongoose

--- src/lib/database/mongoose/models.ts
+++ src/lib/database/mongoose/models.ts
@@ -1,143 +1,132 @@
-import mongoose, { Schema, model, SchemaTypes } from 'mongoose';
+import mongoose from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
 	_id: {
-		type: SchemaTypes.ObjectId,
+		type: Schema.Types.ObjectId,
 		required: true,
 	},
-	name: SchemaTypes.String,
-	email: SchemaTypes.String,
-	image: SchemaTypes.String,
-	emailVerified: SchemaTypes.Date,
+	name: String,
+	email: String,
+	image: String,
+	emailVerified: Date,
 });
-const UserModel = model('user', userSchema, 'users', { overwriteModels: true });
-export const User = (mongoose.models['user'] as typeof UserModel) || UserModel;
+export const User = model('user', userSchema, 'users', {
+	overwriteModels: true,
+});
 
 const sessionSchema = new Schema({
 	sessionToken: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 	expires: {
-		type: SchemaTypes.Date,
+		type: Date,
 		required: true,
 	},
 	userId: {
-		type: SchemaTypes.ObjectId,
+		type: Schema.Types.ObjectId,
 		required: true,
 		ref: 'user',
 	},
 });
-const SessionModel = model('session', sessionSchema, 'sessions', {
+export const Session = model('session', sessionSchema, 'sessions', {
 	overwriteModels: true,
 });
-export const Session =
-	(mongoose.models['session'] as typeof SessionModel) || SessionModel;
 
 const accountSchema = new Schema({
 	provider: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 	type: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 	providerAccountId: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 	access_token: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 	token_type: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 	expires_at: {
-		type: SchemaTypes.Number,
+		type: Number,
 		required: true,
 	},
 	refresh_token: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
-	scope: SchemaTypes.String,
+	scope: String,
 	userId: {
-		type: SchemaTypes.ObjectId,
+		type: Schema.Types.ObjectId,
 		required: true,
 		ref: 'user',
 	},
 });
-const AccountModel = model('account', accountSchema, 'accounts', {
+export const Account = model('account', accountSchema, 'accounts', {
 	overwriteModels: true,
 });
-export const Account =
-	(mongoose.models['account'] as typeof AccountModel) || AccountModel;
 
 const globalStatusPointerSchema = new Schema({
 	current: {
-		type: SchemaTypes.ObjectId,
+		type: Schema.Types.ObjectId,
 		required: true,
 		ref: 'globalStatus',
 	},
 });
-const GlobalStatusPointerModel = model(
+export const GlobalStatusPointer = model(
 	'globalStatusPointer',
 	globalStatusPointerSchema,
 	'globalStatusPointers',
 	{ overwriteModels: true }
 );
-export const GlobalStatusPointer =
-	(mongoose.models['globalStatusPointer'] as typeof GlobalStatusPointerModel) ||
-	GlobalStatusPointerModel;
 
 const globalStatusSchema = new Schema({
 	active: {
-		type: SchemaTypes.Number,
+		type: Number,
 		required: true,
 	},
 	status: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 	statusType: {
-		type: SchemaTypes.String,
+		type: String,
 		enum: ['ok', 'severe', 'concern'],
 		required: true,
 	},
 });
-const GlobalStatusModel = model(
+export const GlobalStatus = model(
 	'globalStatus',
 	globalStatusSchema,
 	'globalStatuses',
 	{ overwriteModels: true }
 );
-export const GlobalStatus =
-	(mongoose.models['globalStatus'] as typeof GlobalStatusModel) ||
-	GlobalStatusModel;
 
 const contactMessageSchema = new Schema({
 	ip: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 	name: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 	message: {
-		type: SchemaTypes.String,
+		type: String,
 		required: true,
 	},
 });
-const ContactMessageModel = model(
+export const ContactMessage = model(
 	'contactMessage',
 	contactMessageSchema,
 	'contactMessages',
 	{ overwriteModels: true }
 );
-export const ContactMessage =
-	(mongoose.models['contactMessage'] as typeof ContactMessageModel) ||
-	ContactMessageModel;
