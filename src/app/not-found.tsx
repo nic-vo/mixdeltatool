56c6bb0ab230ebd5da57228c8a4ef03b58510a52
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-20 03:46:57 -0700

    Tailwindify 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,9 +2,15 @@ import Link from 'next/link';
 
 const NotFound = () => {
 	return (
-		<div>
-			<h2>We couldn&apos;t find what you&apos;re looking for.</h2>
-			<Link href='/'>&larr; Return home</Link>
+		<div className='relative z-10 h-screen w-full flex flex-col items-center justify-center gap-8'>
+			<h2 className='font-cabin text-7xl text-center w-10/12'>
+				We couldn&apos;t find what you&apos;re looking for.
+			</h2>
+			<Link
+				href='/'
+				className='p-8 text-3xl font-thin'>
+				&larr; Return home
+			</Link>
 		</div>
 	);
 };
