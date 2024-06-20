26e3d80eccb35b100b83fb744a870e967b7b07bc
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-20 02:16:03 -0700

    Adjust how Redis client throws in dev mode

--- src/lib/database/redis/client.ts
+++ src/lib/database/redis/client.ts
@@ -1,11 +1,9 @@
-import { CustomError } from '@/lib/errors';
 import { Redis } from '@upstash/redis';
 
 const url = process.env.REDIS_EAST_URL;
 const token = process.env.REDIS_EAST_TOKEN;
 
-if (!url || !token)
-	throw new CustomError(500, 'There was an error authenticating your request');
+if (!url || !token) throw new Error('Missing redis info');
 
 const redisClient = new Redis({ url, token });
 
