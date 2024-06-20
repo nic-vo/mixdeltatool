5c0a10dd9d44bb3909d0e837ee911c5eb706de7c
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-20 03:40:35 -0700

    Replace all responses with badResponse
    Remove unnecessary rl call

--- src/app/api/snoisses/[obfus]/route.ts
+++ src/app/api/snoisses/[obfus]/route.ts
@@ -1,13 +1,7 @@
 import mongoosePromise, { Session } from '@/lib/database/mongoose';
 import checkAndUpdateEntry from '@/lib/database/redis/ratelimiting';
 import { handlerWithTimeoutAndAuth } from '@/lib/misc/helpers';
-import {
-	CreatedResponse,
-	FetchErrorResponse,
-	NotFoundResponse,
-	RateLimitResponse,
-	ServerErrorResponse,
-} from '@/lib/returners';
+import badResponse, { CreatedResponse } from '@/lib/returners';
 import { OPTIONS } from '../../_lib';
 
 import { NextAuthRequest } from 'next-auth/lib';
@@ -22,29 +16,23 @@ export const maxDuration = 30;
 export { OPTIONS };
 
 export const POST = handlerWithTimeoutAndAuth(
-	maxDuration,
+	{
+		maxDuration,
+		rateLimit: {
+			RATE_LIMIT_DECAY_SECONDS,
+			RATE_LIMIT_PREFIX,
+			RATE_LIMIT_ROLLING_LIMIT,
+		},
+	},
 	async (req: NextAuthRequest, { params }: AppRouteHandlerFnContext) => {
 		if (
-			process.env.GLOBAL_SAFETY === 'ON' ||
 			!params ||
 			params['obfus'] !== process.env.SESSION_CRON_ROUTE ||
 			req.headers.get('Authorization') !== process.env.SESSION_CRON_SECRET
 		)
-			return NotFoundResponse();
+			return badResponse(404);
 
-		const { ip } = req;
-		if (!ip) return ServerErrorResponse();
-		try {
-			const rateLimit = await checkAndUpdateEntry({
-				ip,
-				prefix: RATE_LIMIT_PREFIX,
-				rollingLimit: RATE_LIMIT_ROLLING_LIMIT,
-				rollingDecaySeconds: RATE_LIMIT_DECAY_SECONDS,
-			});
-			if (rateLimit !== null) return RateLimitResponse(rateLimit);
-		} catch {
-			return FetchErrorResponse('There was an error');
-		}
+		if (!req.auth) return badResponse(401);
 
 		try {
 			await mongoosePromise();
@@ -57,7 +45,9 @@ export const POST = handlerWithTimeoutAndAuth(
 			// }).exec();
 			// console.log(deleted);
 		} catch {
-			return ServerErrorResponse('There was an error with our servers');
+			return badResponse(500, {
+				message: 'There was an error with our servers',
+			});
 		}
 		return CreatedResponse('Initialized');
 	}
