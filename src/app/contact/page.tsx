1bfbea1c7370a09d1e94cb025a1cd2f56ce30539
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-29 03:20:46 -0700

    Place next/script for hcaptcha in page
    Included in static page bundle
    Not tied to post-hydration hopefully
    Use global components

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,40 +1,29 @@
-import local from '@/styles/contact.module.scss';
-import Link from 'next/link';
-import ContactForm from './_components/form';
+import ContactForm, { HCaptchaBlock } from './_components/form';
+import { GlobalMain, InlineLink } from '@/components/global/serverComponentUI';
+import { SmallNav } from '../_components/EUA/server';
+import Script from 'next/script';
 
-const Contact = () => {
-	return (
-		<main className='flex flex-col p-4 gap-8 w-full max-w-screen-md m-auto self-center justify-self-center'>
-			<h1 className={local.h1}>Contact us!</h1>
-			<p className='text-center'>
-				You can contact us via this form or by emailing{' '}
-				<a
-					href='mailto:mixdeltatool@gmail.com'
-					target='_blank'
-					className='underline font-bold'>
-					mixdeltatool@gmail.com
-				</a>
-				.
-			</p>
-			<ContactForm />
-			<nav className='w-full flex justify-between p-4 mt-auto'>
-				<Link
-					href='/'
-					prefetch={false}
-					className='p-4 border-b-2 border-b-white hover:bg-white hover:text-black focus-visible:bg-white focus-visible:text-black'>
-					&larr; Back home
-				</Link>
-				<Link
-					href='/tool'
-					prefetch={false}
-					className='p-4 border-b-2 border-b-white hover:bg-white hover:text-black focus-visible:bg-white focus-visible:text-black'>
-					To the tool &rarr;
-				</Link>
-			</nav>
-		</main>
-	);
-};
-
+const Contact = () => (
+	<GlobalMain className='mt-8 gap-8'>
+		<Script src='https://js.hcaptcha.com/1/api.js?render=explicit' />
+		<h1 className='font-cabin text-5xl lg:text-8xl font-black text-center'>
+			Comments? Questions?
+		</h1>
+		<ContactForm>
+			<HCaptchaBlock />
+		</ContactForm>
+		<p className='text-center'>
+			You can also email us at{' '}
+			<InlineLink
+				href='mailto:mixdeltatool@gmail.com'
+				target='_blank'>
+				mixdeltatool@gmail.com
+			</InlineLink>
+			.
+		</p>
+		<SmallNav />
+	</GlobalMain>
+);
 export default Contact;
 
 export const metadata = {
