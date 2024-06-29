67e0278680f83e9db2b0391e20692f99f361284b
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-28 23:42:01 -0700

    Add some class strings for common looks to be used with global components

---
+++ src/consts/buttonStates.ts
@@ -0,0 +1,8 @@
+export const hitsSpotify =
+	'before:!bg-green-400 focus-visible:border-green-400 hover:border-green-400';
+
+export const localNavigation =
+	'before:!bg-pinkredlight focus-visible:border-pinkredlight hover:border-pinkredlight';
+
+export const flippedSlider =
+	'before:translate-x-full hover:before:translate-x-0 focus-visible:before:translate-x-0';
