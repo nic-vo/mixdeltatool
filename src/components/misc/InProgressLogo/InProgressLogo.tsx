eda28968a0518368ac1cb68c9218410489f5cee2
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-29 02:28:43 -0700

    Adjust loading logo for aria attr and make it tailwind adjustable

--- src/components/misc/InProgressLogo/InProgressLogo.tsx
+++ src/components/misc/InProgressLogo/InProgressLogo.tsx
@@ -1,12 +1,17 @@
-import LogoToAnimate from '../LogoToAnimate/LogoToAnimate';
+import LogoToAnimate from '../LogoToAnimate';
 
 import local from './InProgressLogo.module.scss';
 
-const InProgressLogo = () => {
-	return <LogoToAnimate
-		left={[local.left]}
-		middle={[local.middle]}
-		right={[local.right]} />
-}
+const InProgressLogo = (props: { twSize?: string }) => {
+	return (
+		<LogoToAnimate
+			main={[props.twSize ?? '']}
+			left={[local.left]}
+			middle={[local.middle]}
+			right={[local.right]}
+			aria-hidden
+		/>
+	);
+};
 
 export default InProgressLogo;
