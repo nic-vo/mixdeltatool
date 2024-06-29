cc113f6fd1b2dca5938380f7f50e3df67f602920
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-29 03:16:51 -0700

    Use global components for 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,16 @@
-import Link from 'next/link';
+import {
+	GlobalBlockLink,
+	GlobalMain,
+} from '@/components/global/serverComponentUI';
 
 const NotFound = () => {
 	return (
-		<div className='relative z-10 h-screen w-full flex flex-col items-center justify-center gap-8'>
+		<GlobalMain>
 			<h2 className='font-cabin text-7xl text-center w-10/12'>
-				We couldn&apos;t find what you&apos;re looking for.
+				We couldn&apos;t find that.
 			</h2>
-			<Link
-				href='/'
-				className='p-8 text-3xl font-thin'>
-				&larr; Return home
-			</Link>
-		</div>
+			<GlobalBlockLink href='/'>&larr; Return home</GlobalBlockLink>
+		</GlobalMain>
 	);
 };
 
