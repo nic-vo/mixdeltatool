2ac4bb7b4f92212ba98fd3d1f61dd0e483173616
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-29 03:13:18 -0700

    Prettify

--- src/state/differFetchStateSlice.ts
+++ src/state/differFetchStateSlice.ts
@@ -2,14 +2,14 @@ import { createSlice } from '@reduxjs/toolkit';
 import {
 	differOperationAsync,
 	retrieveSpecificAsync,
-	retrieveUserListsAsync
+	retrieveUserListsAsync,
 } from './thunks';
 
 import { LoadingState } from './state';
 
 const initialFetchState: LoadingState = {
 	loading: false,
-	error: null
+	error: null,
 };
 
 const differFetchStateSlice = createSlice({
@@ -17,29 +17,39 @@ const differFetchStateSlice = createSlice({
 	initialState: initialFetchState,
 	reducers: {},
 	extraReducers: (builder) => {
-		builder.addCase(retrieveSpecificAsync.pending, (state) => {
-			state.loading = true;
-		}).addCase(retrieveUserListsAsync.pending, (state) => {
-			state.loading = true;
-		}).addCase(differOperationAsync.pending, (state) => {
-			state.loading = true;
-			state.error = null;
-		}).addCase(retrieveSpecificAsync.fulfilled, (state) => {
-			state.loading = false;
-		}).addCase(retrieveUserListsAsync.fulfilled, (state) => {
-			state.loading = false;
-		}).addCase(differOperationAsync.fulfilled, (state) => {
-			state.loading = false;
-		}).addCase(retrieveSpecificAsync.rejected, (state) => {
-			state.loading = false;
-		}).addCase(retrieveUserListsAsync.rejected, (state) => {
-			state.loading = false;
-		}).addCase(differOperationAsync.rejected, (state, action) => {
-			state.loading = false;
-			state.error = action.error.message ?
-				action.error.message : 'Bad response from server';
-		});
-	}
+		builder
+			.addCase(retrieveSpecificAsync.pending, (state) => {
+				state.loading = true;
+			})
+			.addCase(retrieveUserListsAsync.pending, (state) => {
+				state.loading = true;
+			})
+			.addCase(differOperationAsync.pending, (state) => {
+				state.loading = true;
+				state.error = null;
+			})
+			.addCase(retrieveSpecificAsync.fulfilled, (state) => {
+				state.loading = false;
+			})
+			.addCase(retrieveUserListsAsync.fulfilled, (state) => {
+				state.loading = false;
+			})
+			.addCase(differOperationAsync.fulfilled, (state) => {
+				state.loading = false;
+			})
+			.addCase(retrieveSpecificAsync.rejected, (state) => {
+				state.loading = false;
+			})
+			.addCase(retrieveUserListsAsync.rejected, (state) => {
+				state.loading = false;
+			})
+			.addCase(differOperationAsync.rejected, (state, action) => {
+				state.loading = false;
+				state.error = action.error.message
+					? action.error.message
+					: 'Bad response from server';
+			});
+	},
 });
 
 export default differFetchStateSlice.reducer;
