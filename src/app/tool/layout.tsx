ba59db4731b8f76bc13310f0f4490227cc595c41
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-29 22:58:05 -0700

    Init layout for all tool routes

---
+++ src/app/tool/layout.tsx
@@ -0,0 +1,46 @@
+import { auth, saSignOut } from '@/auth';
+import {
+	GlobalButton,
+	GlobalMain,
+	GlobalBlockLink,
+} from '@/components/global/serverComponentUI';
+import { ToolHeading } from './_components/server';
+import {
+	flippedSlider,
+	hitsSpotify,
+	localNavigation,
+} from '@/consts/buttonStates';
+import { redirect } from 'next/navigation';
+
+const ToolRoot = async () => {
+	const session = await auth();
+	if (!session) redirect(`/api/auth/signin`);
+
+	return (
+		<>
+			<GlobalMain className='m-auto'>
+				<ToolHeading>
+					Welcome,{' '}
+					{(session.user &&
+						(session.user?.name ?? session.user?.email?.split('@')[0])) ??
+						'Stranger'}
+					!
+				</ToolHeading>
+				<form action={saSignOut}>
+					<GlobalButton
+						type='submit'
+						className={hitsSpotify}>
+						Sign out
+					</GlobalButton>
+				</form>
+				<GlobalBlockLink
+					href='/'
+					className={localNavigation + ' ' + flippedSlider}>
+					&larr; Return Home
+				</GlobalBlockLink>
+			</GlobalMain>
+		</>
+	);
+};
+
+export default ToolRoot;
