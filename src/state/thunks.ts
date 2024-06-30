44a1c9e254504a7d11dc6ed93e4d78a33b5ef10f
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-06-29 19:17:48 -0700

    Remove try/catch nesting from thunks
    Redux will reject them anyway

--- src/state/thunks.ts
+++ src/state/thunks.ts
@@ -8,101 +8,88 @@ import {
 } from '@/components/spotify/types';
 import { RootState } from './state';
 
+/**
+ * ANY ERRORS THROWN AS { message: string }
+ */
+
 export const retrieveSpecificAsync = createAsyncThunk(
 	'specificPlaylists/retrieveSpecificAsync',
-	async (params: { url: string }) => {
+	async (url: string) => {
+		// Pass e.target.value from simple form
+		// Check input before fetching
+		const splitBegin = url.split('.com/')[1];
+		if (splitBegin === undefined)
+			throw { message: 'There is an error with this link.' };
+
+		const type = splitBegin.split('/')[0] as 'album' | 'playlist';
+		const id = splitBegin.split('/')[1].split('?si')[0];
+
+		// https://open.spotify.com/playlist/4wjsfnoRSCNg0BAV4nvSj6?si=35d3a5a4c6f5493d
+		// https://open.spotify.com/album/12DmuRtZNTx84ELHKD3VGL?si=d79c7c1965564e77
+		if ((type !== 'album' && type !== 'playlist') || id.length !== 22)
+			throw { message: 'There is an error with this link.' };
+
+		let response, json;
 		try {
-			// Check input before fetching
-			const splitBegin = params.url.split('.com/')[1];
-			if (splitBegin === undefined)
-				throw { message: 'There is an error with this link.' };
-
-			const type = splitBegin.split('/')[0] as 'album' | 'playlist';
-			const id = splitBegin.split('/')[1].split('?si')[0];
-			if ((type !== 'album' && type !== 'playlist') || id === undefined)
-				throw { message: 'There is an error with this link.' };
-
-			let response;
-			try {
-				response = await fetch(
-					`/api/spotify/getSpecific?id=${id}&type=${type}`
-				);
-			} catch {
-				throw { message: 'There was an error reaching our servers' };
-			}
-			const jsoned = await response.json();
-			// Return early if okay
-			if (response.ok) return jsoned as MyPlaylistObject;
-			// Any errors get thrown as expected {message: string}
-			if (response.status === 401) signIn();
-			throw jsoned as { message: string };
-		} catch (e: any) {
-			// In case I missed something
-			throw e.message ? e.message : { message: 'Bad response from server' };
+			response = await fetch(`/api/spotify/specific?id=${id}&type=${type}`);
+			json = await response.json();
+		} catch {
+			throw { message: 'There was an error reaching our servers' };
 		}
+
+		if (response.ok) return json as MyPlaylistObject;
+		if (response.status === 401) signIn();
+		throw json as { message: string };
 	}
 );
 
 export const retrieveUserListsAsync = createAsyncThunk(
 	'userPlaylists/retrieveUserListsAsync',
-	async (_, thunkAPI) => {
-		const { userPlaylists } = thunkAPI.getState() as RootState;
-		const { page } = userPlaylists;
+	async (_, api) => {
+		const {
+			userPlaylists: { page },
+		} = api.getState() as RootState;
 		if (page === null) throw { message: "You've reached the end." };
+
+		let response, json;
 		try {
-			let response;
-			try {
-				response = await fetch(`/api/spotify/getUser?page=${page}`);
-			} catch {
-				throw { message: 'There was an error reaching our servers' };
-			}
-			// Return early if okay
-			const jsoned = await response.json();
-			if (response.ok) return jsoned as MyUserAPIRouteResponse;
-			// Any errors get thrown as expected {message: string}
-			if (response.status === 401) signIn();
-			throw jsoned as { message: string };
-		} catch (e: any) {
-			// In case I missed something
-			throw e.message ? e.message : { message: 'Bad response from server' };
+			response = await fetch(`/api/spotify/user?page=${page}`);
+			json = await response.json();
+		} catch {
+			throw { message: 'There was an error reaching our servers' };
 		}
+
+		if (response.ok) return json as MyUserAPIRouteResponse;
+		if (response.status === 401) signIn();
+		throw json as { message: string };
 	}
 );
 
 export const differOperationAsync = createAsyncThunk(
 	'differForm/differOperationAsync',
-	async (_, thunkAPI) => {
-		try {
-			const { differForm: df } = thunkAPI.getState() as RootState;
+	async (_, api) => {
+		const { differForm: df } = api.getState() as RootState;
 
-			const { target, differ, type, keepImg } = df;
-			if (target === '') throw { message: 'Target missing' };
-			if (differ === '') throw { message: 'Differ missing' };
-			if (type === '') throw { message: 'Type missing' };
-			const body = {
-				target,
-				differ,
-				type,
-				keepImg,
-				newName: df.newName !== '' ? df.newName : null,
-				newDesc: df.newDesc !== '' ? df.newDesc : null,
-			};
-			const response = await fetch('/api/spotify/create', {
-				method: 'POST',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify(body),
-			});
-			const jsoned = await response.json();
-			if (response.ok) return jsoned as differRouteResponse;
-			if (response.status === 401) signIn();
-			throw jsoned as { message: string };
-		} catch (e: any) {
-			throw {
-				message:
-					e.message && typeof e.message === 'string'
-						? e.message
-						: 'Received a bad response from the server',
-			};
-		}
+		const { target, differ, type, keepImg } = df;
+		if (target === '') throw { message: 'Target missing' };
+		if (differ === '') throw { message: 'Differ missing' };
+		if (type === '') throw { message: 'Type missing' };
+		const body = {
+			target,
+			differ,
+			type,
+			keepImg,
+			newName: df.newName !== '' ? df.newName : null,
+			newDesc: df.newDesc !== '' ? df.newDesc : null,
+		};
+		const response = await fetch('/api/spotify/create', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(body),
+		});
+		const jsoned = await response.json();
+		if (response.ok) return jsoned as differRouteResponse;
+		if (response.status === 401) signIn();
+		throw jsoned as { message: string };
 	}
 );
