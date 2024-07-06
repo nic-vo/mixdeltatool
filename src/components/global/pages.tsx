79e7f16f158a2ac3a67122579a8f5ce2f3c649d0
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-06 02:05:03 -0700

    Loading page can now accept custom loading text

--- src/components/global/pages.tsx
+++ src/components/global/pages.tsx
@@ -1,22 +1,29 @@
 import { InProgressLogo } from '../misc';
 
-export const GlobalLoading = () => (
+export const GlobalLoading = (props: { display?: string }) => (
 	<main
-		className='relative z-10 flex flex-col justify-center gap-8 items-center m-auto'
-		aria-busy>
-		<h2 className='font-hind font-light text-center text-5xl animate-pulse'>
-			Loading<span className='inline-block animate-pulse'>.</span>
-			<span
-				className='inline-block animate-pulse'
-				style={{ animationDelay: '0.2s' }}>
-				.
-			</span>
-			<span
-				className='inline-block animate-pulse'
-				style={{ animationDelay: '0.4s' }}>
-				.
-			</span>
-		</h2>
+		className='relative z-10 flex flex-col justify-center gap-8 items-center flex-grow'
+		aria-busy
+		aria-live='polite'>
+		<h1 className='font-hind font-light text-center text-5xl animate-pulse'>
+			{props.display ? (
+				props.display
+			) : (
+				<>
+					Loading<span className='inline-block animate-pulse'>.</span>
+					<span
+						className='inline-block animate-pulse'
+						style={{ animationDelay: '0.2s' }}>
+						.
+					</span>
+					<span
+						className='inline-block animate-pulse'
+						style={{ animationDelay: '0.4s' }}>
+						.
+					</span>
+				</>
+			)}
+		</h1>
 		<InProgressLogo />
 	</main>
 );
