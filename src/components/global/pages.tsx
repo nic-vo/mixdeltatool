717458ebf83fd2d0ee426cca37b8fb348af6f2b3
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-06 02:07:10 -0700

    Switch to GlobalMain container

--- src/components/global/pages.tsx
+++ src/components/global/pages.tsx
@@ -1,29 +1,24 @@
 import { InProgressLogo } from '../misc';
+import { GlobalMain } from './serverComponentUI';
 
-export const GlobalLoading = (props: { display?: string }) => (
-	<main
-		className='relative z-10 flex flex-col justify-center gap-8 items-center flex-grow'
+export const GlobalLoading = ({ display }: { display?: string }) => (
+	<GlobalMain
 		aria-busy
 		aria-live='polite'>
-		<h1 className='font-hind font-light text-center text-5xl animate-pulse'>
-			{props.display ? (
-				props.display
-			) : (
-				<>
-					Loading<span className='inline-block animate-pulse'>.</span>
-					<span
-						className='inline-block animate-pulse'
-						style={{ animationDelay: '0.2s' }}>
-						.
-					</span>
-					<span
-						className='inline-block animate-pulse'
-						style={{ animationDelay: '0.4s' }}>
-						.
-					</span>
-				</>
-			)}
+		<h1 className='font-cabin font-light text-center text-5xl animate-pulse'>
+			Loading<span className='inline-block animate-pulse'>.</span>
+			<span
+				className='inline-block animate-pulse'
+				style={{ animationDelay: '0.2s' }}>
+				.
+			</span>
+			<span
+				className='inline-block animate-pulse'
+				style={{ animationDelay: '0.4s' }}>
+				.
+			</span>
 		</h1>
+		{display && <p>{display}</p>}
 		<InProgressLogo />
-	</main>
+	</GlobalMain>
 );
