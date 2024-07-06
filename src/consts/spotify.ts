aad7e967e917f4d13b551fcff6171a46ab10368e
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-05 23:52:49 -0700

    Remove login window const

--- src/consts/spotify.ts
+++ src/consts/spotify.ts
@@ -3,7 +3,6 @@ import SpotifyLogo from './Spotify_Logo_RGB_White.png';
 
 export const SPOT_URL_BASE = 'https://api.spotify.com/v1/';
 export const SPOT_PLAYLIST_PAGE_LIMIT = 49;
-export const SPOT_LOGIN_WINDOW = 50 * 60;
 export const CLIENT_DIFF_TYPES = {
 	stu: 'Only tracks that are in BOTH the target and differ.',
 	odu: 'Tracks that exist only in the differ.',
