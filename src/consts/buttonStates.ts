77e3584fca4ee7889fe430d2c539de62908e8790
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-11 15:52:28 -0700

    All button states now style after: instead of before:

--- src/consts/buttonStates.ts
+++ src/consts/buttonStates.ts
@@ -1,8 +1,8 @@
 export const hitsSpotify =
-	'before:!bg-green-400 focus-visible:border-green-400 hover:border-green-400';
+	'after:!bg-green-400 focus-visible:border-green-400 hover:border-green-400';
 
 export const localNavigation =
-	'before:!bg-pinkredlight focus-visible:border-pinkredlight hover:border-pinkredlight';
+	'after:!bg-pinkredlight focus-visible:border-pinkredlight hover:border-pinkredlight';
 
 export const flippedSlider =
-	'before:translate-x-full hover:before:translate-x-0 focus-visible:before:translate-x-0';
+	'after:translate-x-full hover:after:translate-x-0 focus-visible:after:translate-x-0';
