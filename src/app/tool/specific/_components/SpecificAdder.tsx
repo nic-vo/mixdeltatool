fac296580c1b7d5e358d96abf363ae32c96faa27
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-11 16:59:42 -0700

    Switch to common components
    Add Spotify advisory
    Switch to after: styling

--- src/app/tool/specific/_components/SpecificAdder.tsx
+++ src/app/tool/specific/_components/SpecificAdder.tsx
@@ -3,14 +3,18 @@
 import { retrieveSpecificAsync } from '@/state/thunks';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppDispatch, selectSpecificFetch } from '@/state';
-import { SmallStatus } from '../../_components/server';
+import { AdderForm, SmallStatus } from '../../_components/server';
 import { IoClose, IoDownload } from 'react-icons/io5';
 import {
 	GlobalButton,
 	GlobalTextWrapper,
+	InlineLink,
 } from '@/components/global/serverComponentUI';
 import { hitsSpotify } from '@/consts/buttonStates';
 import { clearSpecific } from '@/state/specificPlaylistsSlice';
+import Image from 'next/image';
+import { SpotifyLogo } from '@/consts/spotify';
+import { useRef } from 'react';
 
 export default function SpecificAdder() {
 	const { loading, error } = useSelector(selectSpecificFetch);
@@ -31,17 +35,20 @@ export default function SpecificAdder() {
 	};
 
 	return (
-		<form
+		<AdderForm
 			onSubmit={submitHandler}
-			onReset={resetHandler}
-			className='flex flex-col gap-2'>
+			onReset={resetHandler}>
 			<fieldset
 				disabled={loading}
 				className='grid grid-cols-2 gap-2 gap-y-4'>
 				<label
 					htmlFor='link'
-					className='col-span-full'>
+					className='flex flex-col gap-2 col-span-full'>
+					<span className='font-karla font-light'>
+						Paste a playlist or album share link
+					</span>
 					<input
+						autoComplete='off'
 						type='text'
 						id='link'
 						name='link'
@@ -61,18 +68,32 @@ export default function SpecificAdder() {
 				</GlobalButton>
 				<GlobalButton
 					type='reset'
-					className='flex items-center gap-2 py-2 before:!bg-red-500 hover:border-red-500 focus-visible:border-red-500 disabled:border-white'>
+					className='flex items-center gap-2 py-2 after:!bg-red-500 hover:border-red-500 focus-visible:border-red-500 disabled:border-white'>
 					<IoClose
 						aria-hidden
 						className='relative z-10 block text-2xl'
 					/>{' '}
 					<GlobalTextWrapper>Clear</GlobalTextWrapper>
 				</GlobalButton>
+				<SmallStatus
+					error={error}
+					loading={loading}
+				/>
 			</fieldset>
-			<SmallStatus
-				error={error}
-				loading={loading}
-			/>
-		</form>
+			<p className='flex items-center gap-2 flex-wrap justify-center'>
+				<span className='block relative'>
+					All playlist data is provided by{' '}
+				</span>
+				<InlineLink
+					href='https://open.spotify.com'
+					target='_blank'>
+					<Image
+						src={SpotifyLogo}
+						alt='Spotify'
+						className='h-[71px] w-auto shrink-0'
+					/>
+				</InlineLink>
+			</p>
+		</AdderForm>
 	);
 }
