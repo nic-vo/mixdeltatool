20960e420ab5289899a5fb51188137afc866eb0d
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-11 16:36:58 -0700

    Init components shared by the adders and mixer
    Adjust small status and heading styling too

--- src/app/tool/_components/server.tsx
+++ src/app/tool/_components/server.tsx
@@ -1,4 +1,5 @@
-import { PropsWithChildren } from 'react';
+import { GlobalMain } from '@/components/global/serverComponentUI';
+import { FormEventHandler, HTMLAttributes, PropsWithChildren } from 'react';
 import { IoAlertCircleSharp } from 'react-icons/io5';
 
 export const ToolHeading = ({
@@ -6,7 +7,7 @@ export const ToolHeading = ({
 	className,
 }: PropsWithChildren & { className?: string }) => (
 	<h1
-		className={`font-cabin font-black text-4xl md:text-6xl${
+		className={`font-cabin font-black text-4xl md:text-6xl lg:w-full${
 			className ? ` ${className}` : ''
 		}`}>
 		{children}
@@ -24,11 +25,14 @@ export const SmallStatus = ({
 		aria-live='assertive'
 		aria-busy={loading}
 		role='alert'>
-		<div className='flex gap-2 items-center'>
+		<div className='flex gap-2 items-center min-h-8'>
 			{error ? (
 				<>
-					<IoAlertCircleSharp aria-hidden />
-					<p>{error}</p>
+					<IoAlertCircleSharp
+						aria-hidden
+						className='text-red-500 text-2xl block'
+					/>
+					<p className='block leading-none'>{error}</p>
 				</>
 			) : (
 				<p className={`italic ${loading ? 'text-inherit' : 'text-slate-500'}`}>
@@ -37,4 +41,38 @@ export const SmallStatus = ({
 			)}
 		</div>
 	</div>
+);
+
+export const AdderMain = (props: PropsWithChildren) => (
+	<GlobalMain className='gap-y-8 max-w-screen-2xl'>{props.children}</GlobalMain>
+);
+
+export const ListContainer = (props: PropsWithChildren) => (
+	<section className='relative flex flex-col items-center gap-8 overflow-hidden h-full after:z-10 after:absolute after:w-full after:h-8 after:bottom-0 after:bg-gradient-to-t after:from-black after:to-transparent'>
+		{props.children}
+	</section>
+);
+
+export const ListUL = (
+	props: PropsWithChildren & Omit<HTMLAttributes<HTMLUListElement>, 'className'>
+) => (
+	<ul
+		{...props}
+		className='relative flex w-full justify-center flex-wrap gap-8 overflow-auto outline-white focus-visible:outline -outline-offset-2 pr-2 pb-8 rounded-xl '>
+		{props.children}
+	</ul>
+);
+
+export const AdderForm = (
+	props: PropsWithChildren & {
+		onReset?: FormEventHandler;
+		onSubmit: FormEventHandler;
+	}
+) => (
+	<form
+		onSubmit={props.onSubmit}
+		onReset={props.onReset}
+		className='flex flex-col gap-2'>
+		{props.children}
+	</form>
 );
