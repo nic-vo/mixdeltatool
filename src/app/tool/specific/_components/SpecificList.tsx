e4174e8820b2149654723eb2fc1c270e889d11a9
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-11 17:00:27 -0700

    Switch to common list components
    Remove Spotify advisory

--- src/app/tool/specific/_components/SpecificList.tsx
+++ src/app/tool/specific/_components/SpecificList.tsx
@@ -2,11 +2,9 @@
 
 import { useSelector } from 'react-redux';
 import { selectSpecificPlaylists } from '@/state';
-import ListItem from '../../_components/dynamic/ListItem';
+import ListItem from '../../_components/ListItem';
 import { useEffect, useState } from 'react';
-import { InlineLink } from '@/components/global/serverComponentUI';
-import Image from 'next/image';
-import { SpotifyLogo } from '@/consts/spotify';
+import { ListContainer, ListUL } from '../../_components/server';
 
 export default function SpecificList() {
 	const [first, setFirst] = useState(true);
@@ -15,41 +13,25 @@ export default function SpecificList() {
 	useEffect(() => setFirst(false), []);
 
 	return (
-		<section className='flex flex-col items-center gap-8 flex-shrink overflow-hidden w-full'>
+		<ListContainer>
 			<hgroup className='flex flex-col gap-8 items-center'>
-				<h2 className='font-light text-3xl text-center w-max'>
+				<h2 className='font-karla font-extralight text-3xl text-center w-max'>
 					Stored Playlists
 				</h2>
-				<p className='flex items-center gap-2 flex-wrap justify-center'>
-					<span className='block relative'>
-						All playlist data is provided by{' '}
-					</span>
-					<InlineLink
-						href='https://open.spotify.com'
-						target='_blank'>
-						<Image
-							src={SpotifyLogo}
-							alt='Spotify'
-							className='h-[71px] w-auto shrink-0'
-						/>
-					</InlineLink>
-				</p>
 			</hgroup>
 			{first || playlists.length === 0 ? (
 				<p className='text-slate-600'>Nothing here...</p>
 			) : (
-				<ul
-					className='flex w-full justify-center flex-wrap gap-8 h-full overflow-auto outline-white focus-visible:outline -outline-offset-2 p-2 rounded-xl'
-					tabIndex={0}>
+				<ListUL tabIndex={0}>
 					{playlists.map((playlist) => (
 						<li
 							key={playlist.id}
-							className='grow-0 shrink w-full sm:w-auto'>
+							className='w-auto'>
 							<ListItem playlist={playlist} />
 						</li>
 					))}
-				</ul>
+				</ListUL>
 			)}
-		</section>
+		</ListContainer>
 	);
 }
