eadfde6aba8e58d1b5a9e4f4f56a98c777dc2f6e
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-11 19:54:46 -0700

    Redo homepage copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,15 +27,15 @@ export default async function Home() {
 			<section className='w-full *:max-w-prose lg:row-start-2 lg:self-start col-start-2 flex flex-col gap-4 self-end'>
 				<h2 className='font-bold text-5xl font-hind'>About this tool</h2>
 				<p>
-					It&apos;s simple: a user can use MixDelta to identify all similarities
-					and differences between any two playlists. After scanning, it can
-					quickly generate a new playlist based on what the user specifies, such
-					as retaining only unique tracks or including only those present in
-					both playlists.
+					At its core, MixDelta provides a convenient way to compare or
+					differentiate any two albums or playlists. After comparing them, it
+					generates a new playlist based on what the user wants to do with the
+					result of that comparison.
 				</p>
 				<p>
-					MixDelta streamlines managing Spotify playlists by providing a
-					seamless way to compare tracklists.
+					Want to remove any duplicate tracks between playlists? Want ONLY the
+					duplicate tracks? Want to do that process over a few dozen playlists
+					in a quick and orderly fashion? MixDelta steps up to the plate.
 				</p>
 			</section>
 		</GlobalMain>
