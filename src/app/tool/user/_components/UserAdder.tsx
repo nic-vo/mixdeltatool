a64ada45f1eb6eb0afaa3a9cf778bf7689fb4763
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-20 03:24:35 -0700

    Focus userAdder on navigation
    Remove disclaimer
    Remove important because twMerge

--- src/app/tool/user/_components/UserAdder.tsx
+++ src/app/tool/user/_components/UserAdder.tsx
@@ -8,17 +8,16 @@ import { IoDownload } from 'react-icons/io5';
 import {
 	GlobalButton,
 	GlobalTextWrapper,
-	InlineLink,
 } from '@/components/global/serverComponentUI';
 import { hitsSpotify } from '@/consts/buttonStates';
 import { clearUser } from '@/state/userPlaylistsSlice';
-import Image from 'next/image';
-import { SpotifyLogo } from '@/consts/spotify';
+import { useEffect, useRef } from 'react';
 
 export default function UserAdder() {
 	const page = useSelector(selectPage);
 	const { loading, error } = useSelector(selectUserFetch);
 	const dispatch = useDispatch<AppDispatch>();
+	const buttonRef = useRef<HTMLButtonElement>(null);
 
 	const submitHandler = (e: React.FormEvent) => {
 		e.preventDefault();
@@ -30,28 +29,38 @@ export default function UserAdder() {
 		dispatch(clearUser());
 	};
 
+	useEffect(() => {
+		const label = buttonRef.current;
+		if (label) label.focus();
+	}, []);
+
 	return (
 		<AdderForm
 			onSubmit={submitHandler}
 			onReset={resetHandler}>
 			<fieldset
 				disabled={loading}
-				className='flex gap-2'>
+				className='grid grid-cols-2 gap-2 items-end'>
 				<GlobalButton
 					type='submit'
 					disabled={page === null}
-					className={hitsSpotify + ' flex items-center gap-2 !px-2 w-full'}>
+					className={
+						hitsSpotify +
+						' flex items-center gap-2 px-2 w-full focus:text-black focus:border-green-500 focus:after:translate-x-0'
+					}
+					autoFocus
+					ref={buttonRef}>
 					<IoDownload
 						aria-hidden
 						className='relative z-10 block text-2xl'
 					/>{' '}
-					<GlobalTextWrapper suppressHydrationWarning>
+					<GlobalTextWrapper>
 						{page === null ? 'No more!' : 'Retrieve'}
 					</GlobalTextWrapper>
 				</GlobalButton>
 				<GlobalButton
 					type='reset'
-					className='flex items-center gap-2 after:!bg-red-500 hover:border-red-500 focus-visible:border-red-500 disabled:border-white shrink'>
+					className='flex items-center gap-2 after:bg-red-500 hover:border-red-500 focus-visible:border-red-500 hover:text-white focus-visible:text-white h-max'>
 					<IoDownload
 						aria-hidden
 						className='relative z-10 block text-2xl shrink-0'
@@ -63,20 +72,6 @@ export default function UserAdder() {
 				error={error}
 				loading={loading}
 			/>
-			<p className='flex items-center gap-2 flex-wrap justify-center'>
-				<span className='block relative'>
-					All playlist data is provided by{' '}
-				</span>
-				<InlineLink
-					href='https://open.spotify.com'
-					target='_blank'>
-					<Image
-						src={SpotifyLogo}
-						alt='Spotify'
-						className='h-[71px] w-auto shrink-0'
-					/>
-				</InlineLink>
-			</p>
 		</AdderForm>
 	);
 }
