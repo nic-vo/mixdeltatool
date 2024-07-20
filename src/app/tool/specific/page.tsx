e737af0fdf4e07dea66dd3c3fad0067d5a5761f1
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-20 02:58:39 -0700

    Move specificAdder disclaimer to page

--- src/app/tool/specific/page.tsx
+++ src/app/tool/specific/page.tsx
@@ -1,13 +1,28 @@
+import { InlineLink } from '@/components/global/serverComponentUI';
 import { AdderMain, ToolHeading } from '../_components/server';
 import { SpecificAdder, SpecificList } from './_components';
+import Image from 'next/image';
+import { SpotifyLogo } from '@/consts/spotify';
 
 const SpecificDialogue = () => (
 	<AdderMain>
-		<ToolHeading className='col-span-full text-center '>
+		<ToolHeading className='text-center'>
 			Add <span className='text-myteal'>Specific</span> Playlists
 		</ToolHeading>
 		<SpecificAdder />
 		<SpecificList />
+		<p className='flex items-center gap-2 flex-wrap justify-center p-1'>
+			<span className='block relative'>All playlist data is provided by </span>
+			<InlineLink
+				href='https://open.spotify.com'
+				target='_blank'>
+				<Image
+					src={SpotifyLogo}
+					alt='Spotify'
+					className='h-[71px] w-auto shrink-0'
+				/>
+			</InlineLink>
+		</p>
 	</AdderMain>
 );
 
