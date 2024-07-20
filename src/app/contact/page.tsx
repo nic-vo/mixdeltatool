8bf5cd0266e3b4c50d619e0ecc4a85694d9d8ec2
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-20 03:29:05 -0700

    Replace heading with generic heading component

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,14 +1,13 @@
 import ContactForm, { HCaptchaBlock } from './_components/form';
 import { GlobalMain, InlineLink } from '@/components/global/serverComponentUI';
-import { SmallNav } from '../_components/EUA/server';
+import { SmallNav } from '@/components/global/bodyText';
 import Script from 'next/script';
+import { ToolHeading } from '../tool/_components/server';
 
 const Contact = () => (
 	<GlobalMain className='mt-8 gap-4'>
 		<Script src='https://js.hcaptcha.com/1/api.js?render=explicit' />
-		<h1 className='font-cabin text-5xl lg:text-8xl font-black text-center'>
-			Comments? Questions?
-		</h1>
+		<ToolHeading>Comments? Questions?</ToolHeading>
 		<ContactForm>
 			<HCaptchaBlock />
 		</ContactForm>
