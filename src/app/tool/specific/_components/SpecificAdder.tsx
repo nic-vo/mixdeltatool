ed67397c5c5ab64664c8e5a7aa3b01c8db9abc5a
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-20 03:25:41 -0700

    Focus specificAdder on navigate
    Remove disclaimer
    Remove important because twMerge

--- src/app/tool/specific/_components/SpecificAdder.tsx
+++ src/app/tool/specific/_components/SpecificAdder.tsx
@@ -8,17 +8,15 @@ import { IoClose, IoDownload } from 'react-icons/io5';
 import {
 	GlobalButton,
 	GlobalTextWrapper,
-	InlineLink,
 } from '@/components/global/serverComponentUI';
 import { hitsSpotify } from '@/consts/buttonStates';
 import { clearSpecific } from '@/state/specificPlaylistsSlice';
-import Image from 'next/image';
-import { SpotifyLogo } from '@/consts/spotify';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 
 export default function SpecificAdder() {
 	const { loading, error } = useSelector(selectSpecificFetch);
 	const dispatch = useDispatch<AppDispatch>();
+	const labelRef = useRef<HTMLLabelElement>(null);
 	const inputRef = useRef<HTMLInputElement>(null);
 
 	const submitHandler = (e: React.FormEvent) => {
@@ -36,6 +34,11 @@ export default function SpecificAdder() {
 		dispatch(clearSpecific());
 	};
 
+	useEffect(() => {
+		const label = labelRef.current;
+		if (label) label.focus();
+	}, []);
+
 	return (
 		<AdderForm
 			onSubmit={submitHandler}
@@ -45,7 +48,8 @@ export default function SpecificAdder() {
 				className='grid grid-cols-2 gap-2 gap-y-4'>
 				<label
 					htmlFor='link'
-					className='flex flex-col gap-2 col-span-full'>
+					className='flex flex-col gap-2 col-span-full'
+					ref={labelRef}>
 					<span className='font-karla font-light'>
 						Paste a playlist or album share link
 					</span>
@@ -57,7 +61,7 @@ export default function SpecificAdder() {
 						required
 						ref={inputRef}
 						pattern='(\d|\w){22}(\?si=(\d|\w)*){0,1)$'
-						className='transition-all outline-none border-white border w-full bg-transparent focus-visible:bg-white focus-visible:text-black p-2 rounded-xl'
+						className='transition-all outline-none border-2 border-slate-500 focus-visible:border-white w-full bg-transparent  p-2 rounded-xl'
 					/>
 				</label>
 				<GlobalButton
@@ -71,7 +75,7 @@ export default function SpecificAdder() {
 				</GlobalButton>
 				<GlobalButton
 					type='reset'
-					className='flex items-center gap-2 py-2 after:!bg-red-500 hover:border-red-500 focus-visible:border-red-500 disabled:border-white'>
+					className='flex items-center gap-2 py-2 after:bg-red-500 hover:border-red-500 focus-visible:border-red-500 hover:text-white focus-visible:text-white'>
 					<IoClose
 						aria-hidden
 						className='relative z-10 block text-2xl'
@@ -83,20 +87,6 @@ export default function SpecificAdder() {
 					loading={loading}
 				/>
 			</fieldset>
-			<p className='flex items-center gap-2 flex-wrap justify-center'>
-				<span className='block relative'>
-					All playlist data is provided by{' '}
-				</span>
-				<InlineLink
-					href='https://open.spotify.com'
-					target='_blank'>
-					<Image
-						src={SpotifyLogo}
-						alt='Spotify'
-						className='h-[71px] w-auto shrink-0'
-					/>
-				</InlineLink>
-			</p>
 		</AdderForm>
 	);
 }
