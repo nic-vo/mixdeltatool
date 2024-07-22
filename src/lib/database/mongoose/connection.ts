9421a7e04c1ccf1f51d2b685fa84c90bdee0cbbb
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-07-22 03:55:17 -0700

    Cache a mongoose connection in dev
    Return a new one in prod

--- src/lib/database/mongoose/connection.ts
+++ src/lib/database/mongoose/connection.ts
@@ -1,43 +1,46 @@
-import { connect } from 'mongoose';
+import mongoose from 'mongoose';
 
 declare global {
-	var mongoose: {
-		conn: null | typeof import('mongoose');
-		promise: null | Promise<typeof import('mongoose')>;
+	var mongooseConn: {
+		conn: null | mongoose.Mongoose;
+		promise: null | Promise<mongoose.Mongoose>;
 	};
 }
 
 // Init a variable from an existing global variable
-let cached = global.mongoose;
+let cached = global.mongooseConn;
 
 // If that global doesn't actually exist, create a base object
 // with null properties to be added later
 if (!cached) {
-	cached = global.mongoose = { conn: null, promise: null };
+	cached = global.mongooseConn = { conn: null, promise: null };
 }
 
 const mongoosePromise = async () => {
-	if (cached.conn !== null) {
+	if (!process.env.MONGODB_URI || !process.env.MONGODB_DB_NAME)
+		throw new Error('Missing MONGODB details');
+	if (process.env.NODE_ENV === 'development') {
+		if (cached.conn) return cached.conn;
+
+		if (cached.promise === null) {
+			cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+				dbName: process.env.MONGODB_DB_NAME,
+				bufferCommands: false,
+			});
+		}
+		try {
+			cached.conn = await cached.promise;
+		} catch (e) {
+			cached.promise = null;
+			throw e;
+		}
 		return cached.conn;
-	}
-
-	if (!process.env.MONGODB_URI) throw new Error('Missing MONGODB_URI');
-
-	if (cached.promise === null) {
-		cached.promise = connect(process.env.MONGODB_URI!, {
-			dbName: process.env.MONGODB_DB_NAME!,
+	} else {
+		return mongoose.connect(process.env.MONGODB_URI, {
+			dbName: process.env.MONGODB_DB_NAME,
 			bufferCommands: false,
-		}).then((mongoose) => {
-			return mongoose;
 		});
 	}
-
-	try {
-		cached.conn = await cached.promise;
-	} catch (e) {
-		cached.promise = null;
-	}
-	return cached.conn;
 };
 
 export default mongoosePromise;
